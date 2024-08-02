523437da060b2c5acf99727616f5990dfda7ada5
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-08-02 20:58:17 +0300

    Update copyright headers

--- apps/test-app/vite.config.ts
+++ apps/test-app/vite.config.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";

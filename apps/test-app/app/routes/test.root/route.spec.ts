5a9d4f20b5c8a55336c128c0f8a34e84df2d0c49
Mayank <9084735+mayank99@users.noreply.github.com>
2024-08-12 09:07:26 -0400

    fix popout document check and add tests (#8)

---
+++ apps/test-app/app/routes/test.root/route.spec.ts
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { test, expect } from "@playwright/test";
+
+test("default", async ({ page }) => {
+	await page.goto("/test/root", { waitUntil: "domcontentloaded" });
+	await expect(page.locator("h1")).toHaveText("Hello");
+	await expect(page.locator("body")).toHaveScreenshot("shadow.png");
+
+	const popoutPromise = page.waitForEvent("popup");
+	await page.getByRole("button", { name: "Open popout" }).click();
+	const popout = await popoutPromise;
+	await popout.waitForLoadState("domcontentloaded");
+	await expect(popout.locator("body")).toHaveScreenshot("popout.png");
+});

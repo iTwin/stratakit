1257a29f35cef606a2a060518337b230ce39f55a
Mayank <9084735+mayank99@users.noreply.github.com>
2024-08-16 09:08:59 -0400

    add `Divider` component (#14)

---
+++ packages/kiwi-react/src/bricks/Divider.tsx
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import * as React from "react";
+import * as Ariakit from "@ariakit/react";
+
+interface DividerProps extends Ariakit.SeparatorProps<"hr"> {}
+
+export const Divider = React.forwardRef<
+	React.ElementRef<typeof Ariakit.Separator>,
+	DividerProps
+>((props, forwardedRef) => {
+	return <Ariakit.Separator {...props} ref={forwardedRef} />;
+});

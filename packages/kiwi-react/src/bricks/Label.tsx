6088716ec18f79d19db48524de4f5a35f16a5ef8
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2024-08-30 10:28:22 -0400

    Add styling for `Label` (#33)

--- packages/kiwi-react/src/bricks/Label.tsx
+++ packages/kiwi-react/src/bricks/Label.tsx
@@ -3,6 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
+import cx from "classnames";
 import * as Ariakit from "@ariakit/react";
 
 interface LabelProps extends Ariakit.RoleProps<"label"> {}
@@ -11,5 +12,11 @@ export const Label = React.forwardRef<
 	React.ElementRef<typeof Ariakit.Role.label>,
 	LabelProps
 >((props, forwardedRef) => {
-	return <Ariakit.Role.label {...props} ref={forwardedRef} />;
+	return (
+		<Ariakit.Role.label
+			{...props}
+			className={cx("🥝-label", props.className)}
+			ref={forwardedRef}
+		/>
+	);
 });

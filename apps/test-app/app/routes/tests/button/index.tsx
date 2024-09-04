027f8c2d73dd2048b8a2fff123fe6732f4841196
Mayank <mayank99@users.noreply.github.com>
2024-09-04 16:09:30 -0400

    visual tests

--- apps/test-app/app/routes/tests/button/index.tsx
+++ apps/test-app/app/routes/tests/button/index.tsx
@@ -8,7 +8,12 @@ import { useSearchParams } from "@remix-run/react";
 export const handle = { title: "Button" };
 
 export default function Page() {
+	const visual = useSearchParams()[0].get("visual") === "true";
 	const disabled = useSearchParams()[0].get("disabled") === "true";
+
+	if (visual) {
+		return <VisualTest />;
+	}
 
 	return (
 		<>
@@ -21,5 +26,14 @@ export default function Page() {
 				Hello
 			</Button>
 		</>
+	);
+}
+
+function VisualTest() {
+	return (
+		<div style={{ display: "flex", gap: 4 }}>
+			<Button>Solid</Button>
+			<Button variant="ghost">Ghost</Button>
+		</div>
 	);
 }

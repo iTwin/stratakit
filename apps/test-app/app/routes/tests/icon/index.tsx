1ce6aee7aedd06b5e57b24fff8d9e52b7898e25b
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-09-06 12:54:14 +0300

    Add tests

--- apps/test-app/app/routes/tests/icon/index.tsx
+++ apps/test-app/app/routes/tests/icon/index.tsx
@@ -4,31 +4,17 @@
  *--------------------------------------------------------------------------------------------*/
 import { Icon } from "@itwin/kiwi-react/bricks";
 import icon2d from "@itwin/kiwi-icons/2d.svg";
-import arrowIcon from "@itwin/kiwi-icons/arrow.svg";
+import { useSearchParams } from "@remix-run/react";
 
 export const handle = { title: "Icon" };
 
 export default function Page() {
-	return (
-		<>
-			<Icon id="2d" href={icon2d} style={{ fill: "white" }} />
-			<Icon
-				id="2d-regular"
-				size="regular"
-				href={icon2d}
-				style={{ fill: "white" }}
-			/>
-			<Icon
-				id="2d-large"
-				href={icon2d}
-				size="large"
-				style={
-					{
-						fill: "white",
-						"--_kiwi-color-icon-2d-box": "yellow",
-					} as React.CSSProperties
-				}
-			/>
-		</>
-	);
+	const size = useSizeParam();
+	return <Icon size={size} href={icon2d} style={{ fill: "white" }} />;
+}
+
+function useSizeParam() {
+	const sizeParam = useSearchParams()[0].get("size");
+	if (sizeParam === "regular" || sizeParam === "large") return sizeParam;
+	return undefined;
 }

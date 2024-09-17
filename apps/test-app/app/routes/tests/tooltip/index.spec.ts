4ac2bf37671fc94b29ef0bc45d788e6d9bee513e
Richard Hill <richard.hill@wipit.io>
2024-09-17 11:16:23 +0100

    Test to ensure tooltip is hidden initially

--- apps/test-app/app/routes/tests/tooltip/index.spec.ts
+++ apps/test-app/app/routes/tests/tooltip/index.spec.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 import { test, expect } from "@playwright/test";
 
-test("should display tooltip on hover", async ({ page }) => {
+test("On initial load Tooltip should be hidden", async ({ page }) => {
 	// Navigate to the page containing the tooltip
 	await page.goto("/tests/tooltip");
 
@@ -14,28 +14,4 @@ test("should display tooltip on hover", async ({ page }) => {
 	// Ensure the tooltip is hidden initially
 	const tooltip = page.locator("text=Click Me!");
 	await expect(tooltip).toBeHidden();
-
-	// Hover over the trigger element
-	await trigger.hover();
-
-	// Expect the tooltip to be visible
-	await expect(tooltip).toBeVisible();
-});
-
-test("should display tooltip on focus", async ({ page }) => {
-	// Navigate to the page containing the tooltip
-	await page.goto("/tests/tooltip");
-
-	// Locate the element that should trigger the tooltip on focus
-	const trigger = page.getByText("Hello World");
-
-	// Ensure the tooltip is hidden initially
-	const tooltip = page.locator("text=Tooltip Content");
-	await expect(tooltip).toBeHidden();
-
-	// Focus on the trigger element
-	await trigger.focus();
-
-	// Expect the tooltip to be visible
-	await expect(tooltip).toBeVisible();
 });

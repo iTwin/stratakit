7c0e5b91c71e2f919a9a6de8791aa270e6fa9e30
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-09-20 13:00:11 +0300

    Import internal ListItem in test-app.

---
+++ apps/test-app/app/routes/tests/list/index.tsx
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { ListItem } from "@itwin/kiwi-react-internal/src/bricks/ListItem";
+
+export const handle = { title: "List" };
+
+export default function Page() {
+	return <ListItem>Hello</ListItem>;
+}

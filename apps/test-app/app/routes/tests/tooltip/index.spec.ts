6baf5a399adf1730bba6160b4419b6fcacbf3f55
Richard Hill <richard.hill@wipit.io>
2024-10-01 20:05:06 +0100

    Removing tests for sanity.

--- apps/test-app/app/routes/tests/tooltip/index.spec.ts
+++ apps/test-app/app/routes/tests/tooltip/index.spec.ts
@@ -32,48 +32,6 @@ test("Keyboard Focus on related element should display the tooltip", async ({
 	await expect(tooltip).toBeVisible();
 });
 
-test("Mouse In / Hover - should display the tooltip", async ({ page }) => {
-	await page.goto("/tests/tooltip");
-
-	const button = page.getByRole("button");
-	const tooltip = page.getByRole("tooltip");
-
-	await button.hover();
-
-	await expect(tooltip).toBeVisible();
-});
-
-test("Mouse Out / Normal - should hide the tooltip", async ({ page }) => {
-	await page.goto("/tests/tooltip");
-
-	const button = page.getByRole("button");
-	const tooltip = page.getByRole("tooltip");
-
-	await button.hover();
-
-	await expect(tooltip).toBeVisible();
-
-	await page.mouse.move(0, 0);
-
-	await expect(tooltip).toBeHidden();
-});
-
-test("Tooltip should stay displayed during hover (should not hide)", async ({
-	page,
-}) => {
-	await page.goto("/tests/tooltip");
-
-	const button = page.getByRole("button");
-	const tooltip = page.getByRole("tooltip");
-
-	await button.hover();
-	await expect(tooltip).toBeVisible();
-	await tooltip.hover();
-	await page.waitForTimeout(3000);
-
-	await expect(tooltip).toBeVisible();
-});
-
 test("Keyboard loss of Focus on the related element should hide the tooltip", async ({
 	page,
 }) => {

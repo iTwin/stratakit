48bd6ec088cbdc0519115b6717bd39c06ebf337f
Richard Hill <richard.hill@wipit.io>
2024-10-02 11:50:57 +0100

    Re-instate hover but skip on webkit

--- apps/test-app/app/routes/tests/tooltip/index.spec.ts
+++ apps/test-app/app/routes/tests/tooltip/index.spec.ts
@@ -51,6 +51,58 @@ test("Keyboard loss of Focus on the related element should hide the tooltip", as
 	await expect(tooltip).toBeHidden();
 });
 
+test.describe("Tooltip hover behaviour", () => {
+	test.skip(
+		({ browserName }) => browserName === "webkit",
+		"Tooltip does not appear on hover in Webkit inside Docker :(",
+	);
+
+	test("Mouse In / Hover - should display the tooltip", async ({ page }) => {
+		await page.goto("/tests/tooltip");
+
+		const button = page.getByRole("button");
+		const tooltip = page.getByRole("tooltip");
+
+		await button.hover();
+		await expect(tooltip).toBeVisible();
+	});
+
+	test("Mouse Out / Normal - should hide the tooltip", async ({ page }) => {
+		test.skip(
+			({ browserName }) => browserName === "webkit",
+			"Tooltip does not appear on hover in Webkit inside Docker :(",
+		);
+
+		await page.goto("/tests/tooltip");
+
+		const button = page.getByRole("button");
+		const tooltip = page.getByRole("tooltip");
+
+		await button.hover();
+		await expect(tooltip).toBeVisible();
+
+		await page.mouse.move(0, 0);
+		await expect(tooltip).toBeHidden();
+	});
+
+	test("Tooltip should stay displayed during hover (should not hide)", async ({
+		page,
+	}) => {
+		await page.goto("/tests/tooltip");
+
+		const button = page.getByRole("button");
+		const tooltip = page.getByRole("tooltip");
+
+		await button.hover();
+
+		await expect(tooltip).toBeVisible();
+		await tooltip.hover();
+
+		await page.waitForTimeout(3000);
+		await expect(tooltip).toBeVisible();
+	});
+});
+
 test("Is aria-describedby is set correctly ?", async ({ page }) => {
 	await page.goto("/tests/tooltip");
 

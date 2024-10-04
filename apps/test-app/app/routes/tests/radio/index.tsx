694656368d2d7a0ba764a1c7887ea98502c1ea26
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2024-10-04 09:16:33 -0400

    Add styling for `Checkbox` & `Radio` (#38)
    Co-authored-by: Mayank <9084735+mayank99@users.noreply.github.com>
    Co-authored-by: Mayank <mayank99@users.noreply.github.com>

--- apps/test-app/app/routes/tests/radio/index.tsx
+++ apps/test-app/app/routes/tests/radio/index.tsx
@@ -2,7 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { Radio, Label } from "@itwin/kiwi-react/bricks";
+import { Radio, Label, VisuallyHidden } from "@itwin/kiwi-react/bricks";
 import { useSearchParams } from "@remix-run/react";
 import { useId } from "react";
 
@@ -12,9 +12,14 @@ export default function Page() {
 	const [searchParams] = useSearchParams();
 	const defaultValue = searchParams.get("defaultValue");
 	const disabled = searchParams.get("disabled") === "true";
+	const visualTest = searchParams.get("visual") === "true";
 
 	const idA = useId();
 	const idB = useId();
+
+	if (visualTest) {
+		return <VisualTest />;
+	}
 
 	return (
 		<>
@@ -35,6 +40,27 @@ export default function Page() {
 				disabled={disabled}
 			/>
 			<Label htmlFor={idB}>B</Label>
+		</>
+	);
+}
+
+function VisualTest() {
+	const [searchParams] = useSearchParams();
+	const checked = searchParams.get("checked") === "true";
+	const disabled = searchParams.get("disabled") === "true";
+
+	const id = useId();
+
+	return (
+		<>
+			<Radio
+				name="test"
+				value="A"
+				id={id}
+				defaultChecked={checked}
+				disabled={disabled}
+			/>
+			<VisuallyHidden render={<Label htmlFor={id} />}>Toggle me</VisuallyHidden>
 		</>
 	);
 }

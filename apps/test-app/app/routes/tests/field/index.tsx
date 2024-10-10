9b094cc4986ecc0b33fe5ed81802816afe88317b
Nathan Knowler <nathan@knowler.dev>
2024-10-10 10:25:03 -0500

    Use all form controls in `<Field>` examples

--- apps/test-app/app/routes/tests/field/index.tsx
+++ apps/test-app/app/routes/tests/field/index.tsx
@@ -2,20 +2,33 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { Field, Input, Label } from "@itwin/kiwi-react/bricks";
+import {
+	Checkbox,
+	Field,
+	Input,
+	Label,
+	Radio,
+	Textarea,
+} from "@itwin/kiwi-react/bricks";
 
 export default function Page() {
 	return (
 		<form>
-			{/* These should be synced */}
 			<Field>
-				<Label>Name</Label>
-				<Input />
+				<Label>Text example</Label> <Input />
 			</Field>
-			{/* These aren’t synced and right now that’s intentional */}
 			<Field>
-				<Label>Fruit</Label>
-				<Input id="fruit" />
+				<Label>Textarea example</Label> <Textarea />
+			</Field>
+			<Field>
+				<Label>
+					<Checkbox /> Checkbox example
+				</Label>
+			</Field>
+			<Field>
+				<Label>
+					<Radio name="radio" value="radio" /> Radio example
+				</Label>
 			</Field>
 		</form>
 	);

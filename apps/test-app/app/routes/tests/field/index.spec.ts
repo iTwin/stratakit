72cfef556a0f4887e40e1ed9aa80f4190446e7d9
Nathan Knowler <nathan@knowler.dev>
2024-10-10 10:54:06 -0500

    Add tests for `<Field>` label association

---
+++ apps/test-app/app/routes/tests/field/index.spec.ts
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { test, expect } from "@playwright/test";
+
+test.describe("default", () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto("/tests/field");
+	});
+
+	test("wrapping input and label", async ({ page }) => {
+		await expect(page.locator("input:not([type])")).toHaveAccessibleName(
+			"Text example",
+		);
+	});
+
+	test("wrapping textarea and label", async ({ page }) => {
+		await expect(page.locator("textarea")).toHaveAccessibleName(
+			"Textarea example",
+		);
+	});
+
+	test("wrapping checkbox and label", async ({ page }) => {
+		await expect(page.getByRole("checkbox")).toHaveAccessibleName(
+			"Checkbox example",
+		);
+	});
+
+	test("wrapping radio and label", async ({ page }) => {
+		await expect(page.getByRole("radio")).toHaveAccessibleName("Radio example");
+	});
+});

0cf9a3932c1d53f2ddf53c09bb6301ff855747a5
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2024-10-11 10:34:01 -0400

    Add link to tokens on homepage.

--- apps/test-app/app/routes/index.tsx
+++ apps/test-app/app/routes/index.tsx
@@ -46,6 +46,9 @@ export default function Index() {
 				<li>
 					<Anchor render={<Link to="/sandbox" />}>Sandbox</Anchor>
 				</li>
+				<li>
+					<Anchor render={<Link to="/tokens" />}>Tokens</Anchor>
+				</li>
 			</ul>
 
 			<ul className={styles.list}>

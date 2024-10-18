2331226aafa1621306e7331705ff2625dab2e190
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-10-18 15:16:36 +0300

    Add docs

--- packages/kiwi-react/src/bricks/TextField.tsx
+++ packages/kiwi-react/src/bricks/TextField.tsx
@@ -9,6 +9,25 @@ import { Input } from "./Input.js";
 
 interface TextFieldProps extends Ariakit.RoleProps<"div"> {}
 
+/**
+ * Text field component is a container that displays input with additional decorations.
+ * Currently supported decoration components are: `Icon`.
+ *
+ * It must contain `Input` component as a child:
+ * ```tsx
+ * <TextField>
+ * 	<Input />
+ * </TextField>
+ * ```
+ *
+ * To add `Icon` decoration before an input:
+ * ```tsx
+ * <TextField>
+ *  <Icon href={…} />
+ * 	<Input />
+ * </TextField>
+ * ```
+ */
 export const TextField = React.forwardRef<
 	React.ElementRef<"div">,
 	TextFieldProps

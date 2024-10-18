14dbabd92333d75ef4ec0c096a279f2c302d21d8
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-10-18 14:39:28 +0300

    Forward ref.

--- packages/kiwi-react/src/bricks/TextField.tsx
+++ packages/kiwi-react/src/bricks/TextField.tsx
@@ -2,20 +2,25 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+import * as React from "react";
 import type * as Ariakit from "@ariakit/react";
 import cx from "classnames";
 import { Input } from "./Input.js";
 
 interface TextFieldProps extends Ariakit.RoleProps<"div"> {}
 
-export function TextField({ children, className }: TextFieldProps) {
+export const TextField = React.forwardRef<
+	React.ElementRef<"div">,
+	TextFieldProps
+>(({ children, className, ref: _, ...rest }, ref) => {
 	return (
 		<Input
-			className={cx("🥝-text-field", className)}
 			focusable={false}
-			render={<div />}
+			render={<div ref={ref} />}
+			{...rest}
+			className={cx("🥝-text-field", className)}
 		>
 			{children}
 		</Input>
 	);
-}
+});

7063058fe88e61350bb573896246ffae29e2ded5
Mayank <9084735+mayank99@users.noreply.github.com>
2024-10-22 18:05:54 -0400

    remove `basename`

--- apps/test-app/vite.config.ts
+++ apps/test-app/vite.config.ts
@@ -20,7 +20,6 @@ export default defineConfig({
 	...(basename && { base: basename }),
 	plugins: [
 		reactRouter({
-			...(basename && { basename }),
 			ssr: false, // SPA mode for github-pages
 			prerender: true, // prerendering for github-pages
 		}),

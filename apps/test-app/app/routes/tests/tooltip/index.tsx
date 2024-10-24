3a7578f09987d3f8d70554195276d7bda7a2e57b
Richard Hill <RichardHill@users.noreply.github.com>
2024-10-24 16:54:54 +0100

    ARIA - Tooltip Strategy (#93)
    Co-authored-by: Richard Hill <richard.hill@bentley.com>
    Co-authored-by: Mayank <9084735+mayank99@users.noreply.github.com>
    Co-authored-by: Nathan Knowler <nathan@knowler.dev>
    Co-authored-by: GerardasB <10091419+GerardasB@users.noreply.github.com>

--- apps/test-app/app/routes/tests/tooltip/index.tsx
+++ apps/test-app/app/routes/tests/tooltip/index.tsx
@@ -3,13 +3,19 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { Tooltip, Button, VisuallyHidden } from "@itwin/kiwi-react/bricks";
+import { useSearchParams } from "@remix-run/react";
 
 export const handle = { title: "Tooltip" };
 
 export default function Page() {
+	const [searchParams] = useSearchParams();
+	const type =
+		(searchParams.get("type") as "description" | "label" | "none" | null) ??
+		undefined;
+
 	return (
 		<>
-			<Tooltip content="This is the tooltip">
+			<Tooltip content="This is the tooltip" type={type}>
 				<Button>Hover/focus me</Button>
 			</Tooltip>
 

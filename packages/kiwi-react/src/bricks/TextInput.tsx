c922a7773f53becb6de430028bc942fd35245105
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-10-25 14:10:06 +0300

    Re-define the type prop with a smaller string union type.

--- packages/kiwi-react/src/bricks/TextInput.tsx
+++ packages/kiwi-react/src/bricks/TextInput.tsx
@@ -7,7 +7,18 @@ import * as Ariakit from "@ariakit/react";
 import cx from "classnames";
 import { useFieldId } from "./Field.js";
 
-interface TextInputProps extends Ariakit.FocusableProps<"input"> {}
+interface TextInputProps extends Omit<Ariakit.FocusableProps<"input">, "type"> {
+	/**
+	 * Describes a text based [input type](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#input_types)
+	 * based on the value type the user will enter.
+	 *
+	 * @default "text"
+	 */
+	type?: Extract<
+		React.HTMLInputTypeAttribute,
+		"text" | "email" | "password" | "search" | "tel" | "url" | "number"
+	>;
+}
 
 export const TextInput = React.forwardRef<
 	React.ElementRef<"input">,

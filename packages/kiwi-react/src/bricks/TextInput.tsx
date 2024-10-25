9969eb7b7c3b28b2da9460992b014caf6cca0a8e
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-10-25 14:12:52 +0300

    Disallow children

--- packages/kiwi-react/src/bricks/TextInput.tsx
+++ packages/kiwi-react/src/bricks/TextInput.tsx
@@ -7,7 +7,10 @@ import * as Ariakit from "@ariakit/react";
 import cx from "classnames";
 import { useFieldId } from "./Field.js";
 
-interface TextInputProps extends Omit<Ariakit.FocusableProps<"input">, "type"> {
+interface TextInputProps
+	extends Omit<Ariakit.FocusableProps<"input">, "children" | "type"> {
+	/** Input is a [void element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#technical_summary) and no content is permitted. */
+	children?: never;
 	/**
 	 * Describes a text based [input type](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#input_types)
 	 * based on the value type the user will enter.

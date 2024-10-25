50eb229c09e80a6fc22f82d90513bf0e08a9cc9d
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-10-25 13:49:11 +0300

    Rename Input to TextInput

--- packages/kiwi-react/src/bricks/Input.tsx
+++ packages/kiwi-react/src/bricks/Input.tsx
@@ -7,25 +7,26 @@ import * as Ariakit from "@ariakit/react";
 import cx from "classnames";
 import { useFieldId } from "./Field.js";
 
-interface InputProps extends Ariakit.FocusableProps<"input"> {}
+interface TextInputProps extends Ariakit.FocusableProps<"input"> {}
 
-export const Input = React.forwardRef<React.ElementRef<"input">, InputProps>(
-	(props, forwardedRef) => {
-		const fieldId = useFieldId();
+export const TextInput = React.forwardRef<
+	React.ElementRef<"input">,
+	TextInputProps
+>((props, forwardedRef) => {
+	const fieldId = useFieldId();
 
-		return (
-			<Ariakit.Role.input
-				id={fieldId}
-				{...props}
-				className={cx("🥝-input", props.className)}
-				render={
-					<Ariakit.Focusable
-						accessibleWhenDisabled
-						render={props.render || <input />}
-					/>
-				}
-				ref={forwardedRef}
-			/>
-		);
-	},
-);
+	return (
+		<Ariakit.Role.input
+			id={fieldId}
+			{...props}
+			className={cx("🥝-input", props.className)}
+			render={
+				<Ariakit.Focusable
+					accessibleWhenDisabled
+					render={props.render || <input />}
+				/>
+			}
+			ref={forwardedRef}
+		/>
+	);
+});

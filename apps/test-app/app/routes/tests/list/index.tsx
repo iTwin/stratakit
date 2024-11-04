82ba7f75db08fe3f6fb8cc2bc4c5792abaefa640
Nathan Knowler <nathan@knowler.dev>
2024-11-04 11:07:30 -0600

    Use compound component pattern for `ListItem`

--- apps/test-app/app/routes/tests/list/index.tsx
+++ apps/test-app/app/routes/tests/list/index.tsx
@@ -2,7 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { ListItem } from "@itwin/kiwi-react-internal/src/bricks/ListItem";
+import * as ListItem from "@itwin/kiwi-react-internal/src/bricks/ListItem";
 import { Icon } from "@itwin/kiwi-react-internal/src/bricks/Icon";
 import { useSearchParams } from "@remix-run/react";
 
@@ -21,20 +21,20 @@ export default function Page() {
 		<div role="list">
 			{searchParams.has("with-icons") ? (
 				<>
-					<ListItem>
-						<span>Kiwi</span>
+					<ListItem.Root>
+						<ListItem.Content>Kiwi</ListItem.Content>
 						<Icon href={placeholderIcon} />
-					</ListItem>
-					<ListItem>
+					</ListItem.Root>
+					<ListItem.Root>
 						<Icon href={placeholderIcon} />
-						<span>Tomato</span>
-					</ListItem>
+						<ListItem.Content>Tomato</ListItem.Content>
+					</ListItem.Root>
 				</>
 			) : (
 				<>
-					<ListItem>Apple</ListItem>
-					<ListItem>Cherry</ListItem>
-					<ListItem>Kiwi</ListItem>
+					<ListItem.Root>Apple</ListItem.Root>
+					<ListItem.Root>Cherry</ListItem.Root>
+					<ListItem.Root>Kiwi</ListItem.Root>
 				</>
 			)}
 		</div>

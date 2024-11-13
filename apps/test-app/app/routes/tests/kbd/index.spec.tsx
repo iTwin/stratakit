5903bda9ef5a62ea476f44fc6fa3119b633a43a6
Richard Hill <richard.hill@wipit.io>
2024-11-13 14:03:48 +0000

    Adding AXE tests

--- apps/test-app/app/routes/tests/kbd/index.spec.tsx
+++ apps/test-app/app/routes/tests/kbd/index.spec.tsx
@@ -3,9 +3,23 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test("default", async ({ page }) => {
 	await page.goto("/tests/kbd");
-	const divider = page.getByTestId("kbd");
-	expect(divider).not.toBe(null);
+	const kbdComponent = page.getByTestId("kbd");
+	expect(kbdComponent).not.toBe(null);
+});
+
+test.describe("@a11y", () => {
+	test("Axe Page Scan", async ({ page }) => {
+		await page.goto("/tests/kbd");
+
+		const kbdComponent = page.getByTestId("kbd");
+		await expect(kbdComponent).toBeVisible();
+
+		const axe = new AxeBuilder({ page });
+		const accessibilityScan = await axe.analyze();
+		expect(accessibilityScan.violations).toEqual([]);
+	});
 });

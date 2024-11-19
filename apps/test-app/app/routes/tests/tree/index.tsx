3f14ca362a0f124d52962d30f241d54e1d8d4878
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-11-19 10:52:23 +0200

    Add VisualTest

--- apps/test-app/app/routes/tests/tree/index.tsx
+++ apps/test-app/app/routes/tests/tree/index.tsx
@@ -3,13 +3,35 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { useSearchParams } from "@remix-run/react";
-import { Tree } from "@itwin/kiwi-react/bricks";
+import { Icon, IconButton, Tree } from "@itwin/kiwi-react/bricks";
 
 export const handle = { title: "Tree" };
+
+const placeholderIcon = new URL(
+	"@itwin/kiwi-icons/placeholder.svg",
+	import.meta.url,
+).href;
+const expanderIcon = new URL(
+	"@itwin/kiwi-icons/chevron-right.svg",
+	import.meta.url,
+).href;
+const unlockIcon = new URL(
+	"@itwin/kiwi-icons/lock-unlocked.svg",
+	import.meta.url,
+).href;
+const showIcon = new URL(
+	"@itwin/kiwi-icons/visibility-show.svg",
+	import.meta.url,
+).href;
 
 export default function Page() {
 	const [searchParams] = useSearchParams();
 	const active = searchParams.has("active");
+	const visual = searchParams.has("visual");
+
+	if (visual) {
+		return <VisualTest />;
+	}
 	return (
 		<Tree.Root>
 			<Tree.Item content="Item 1" active={active}>
@@ -21,6 +43,24 @@ export default function Page() {
 				<Tree.Item content="Item 2.1" active={active} />
 			</Tree.Item>
 			<Tree.Item content="Item 3" active={active} />
+		</Tree.Root>
+	);
+}
+
+function VisualTest() {
+	return (
+		<Tree.Root>
+			<Tree.Item
+				content={
+					<>
+						<IconButton icon={expanderIcon} label="Expand" variant="ghost" />
+						<Icon href={placeholderIcon} />
+						Item 1
+						<IconButton icon={unlockIcon} label="Unlock" variant="ghost" />
+						<IconButton icon={showIcon} label="Show" variant="ghost" />
+					</>
+				}
+			/>
 		</Tree.Root>
 	);
 }

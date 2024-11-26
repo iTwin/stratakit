b69a50299989132fe5560cc49929ec563a11b801
Richard Hill <RichardHill@users.noreply.github.com>
2024-11-26 14:02:07 +0000

    Update packages/kiwi-react/src/bricks/Kbd.tsx
    Co-authored-by: GerardasB <10091419+GerardasB@users.noreply.github.com>

--- packages/kiwi-react/src/bricks/Kbd.tsx
+++ packages/kiwi-react/src/bricks/Kbd.tsx
@@ -32,7 +32,7 @@ export const KbdKeys = {
 	Tab: "\u21e5 Tab",
 } as const;
 
-interface KbdProps extends React.ComponentPropsWithoutRef<"kbd"> {
+interface KbdProps extends Ariakit.RoleProps<"kbd"> {
 	/**
 	 * Content of the key to be passed as children. Must be a string or one of the `KbdKeys`.
 	 */

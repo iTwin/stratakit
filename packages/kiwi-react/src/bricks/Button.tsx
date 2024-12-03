c70f3f5e974814baf4e323549afb755ed9de6a5a
Mayank <9084735+mayank99@users.noreply.github.com>
2024-12-03 16:40:18 -0500

    support `tone="accent`

--- packages/kiwi-react/src/bricks/Button.tsx
+++ packages/kiwi-react/src/bricks/Button.tsx
@@ -9,18 +9,27 @@ import * as Ariakit from "@ariakit/react";
 interface ButtonProps extends Ariakit.ButtonProps {
 	/** @default "solid" */
 	variant?: "solid" | "outline" | "ghost";
+	/**
+	 * The tone of the button. Most buttons should be neutral.
+	 * Accent buttons can be used to draw attention to the primary action.
+	 *
+	 * **Note:** The `"accent"` tone is only supported for the `"solid"` variant.
+	 *
+	 * @default "neutral"
+	 */
+	tone?: "neutral" | "accent";
 }
 
 export const Button = React.forwardRef<
 	React.ElementRef<typeof Ariakit.Button>,
 	ButtonProps
 >((props, forwardedRef) => {
-	const { variant = "solid", ...rest } = props;
+	const { variant = "solid", tone = "neutral", ...rest } = props;
 	return (
 		<Ariakit.Button
 			accessibleWhenDisabled
 			data-kiwi-variant={variant}
-			data-kiwi-tone="neutral"
+			data-kiwi-tone={tone}
 			{...rest}
 			className={cx("🥝-button", props.className)}
 			ref={forwardedRef}

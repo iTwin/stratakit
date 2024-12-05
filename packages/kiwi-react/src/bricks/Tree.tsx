e0a9d8d35fb3770861d0ea6db6e940159e7ab636
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-12-05 18:22:55 +0200

    Add `Tree.Item` composition components (#158)

--- packages/kiwi-react/src/bricks/Tree.tsx
+++ packages/kiwi-react/src/bricks/Tree.tsx
@@ -6,15 +6,17 @@ import * as React from "react";
 import cx from "classnames";
 import * as Ariakit from "@ariakit/react";
 import * as ListItem from "./ListItem.js";
+import { IconButton } from "./IconButton.js";
+import { Icon } from "./Icon.js";
 
 // ----------------------------------------------------------------------------
 
 interface TreeProps extends Ariakit.RoleProps<"div"> {}
 
-export const Tree = React.forwardRef<React.ElementRef<"div">, TreeProps>(
+const Tree = React.forwardRef<React.ElementRef<"div">, TreeProps>(
 	(props, forwardedRef) => {
 		return (
-			<Ariakit.Role.div {...props} role="tree" ref={forwardedRef}>
+			<Ariakit.Role.div {...props} role="list" ref={forwardedRef}>
 				{props.children}
 			</Ariakit.Role.div>
 		);
@@ -31,56 +33,141 @@ interface TreeItemProps extends Omit<Ariakit.RoleProps<"div">, "content"> {
 	expanded?: boolean;
 }
 
-export const TreeItem = React.forwardRef<
-	React.ElementRef<"div">,
-	TreeItemProps
->((props, forwardedRef) => {
-	const { selected, content, children, className, expanded, style, ...rest } =
-		props;
+const TreeItem = React.forwardRef<React.ElementRef<"div">, TreeItemProps>(
+	(props, forwardedRef) => {
+		const { selected, content, children, className, expanded, style, ...rest } =
+			props;
 
-	const parentContext = React.useContext(TreeItemContext);
-	const level = parentContext ? parentContext.level + 1 : 1;
-	return (
-		<TreeItemContext.Provider
-			value={React.useMemo(
-				() => ({
-					level,
-				}),
-				[level],
-			)}
-		>
-			<ListItem.Root
-				{...rest}
-				aria-expanded={expanded}
-				aria-selected={selected}
-				aria-level={level}
-				className={cx("🥝-tree-item", className)}
-				style={
-					{
-						...style,
-						"--🥝tree-item-level": level,
-					} as React.CSSProperties
-				}
-				role="treeitem"
-				ref={forwardedRef}
+		const parentContext = React.useContext(TreeItemContext);
+		const level = parentContext ? parentContext.level + 1 : 1;
+		return (
+			<TreeItemContext.Provider
+				value={React.useMemo(
+					() => ({
+						level,
+						expanded,
+					}),
+					[level, expanded],
+				)}
 			>
-				{content}
-			</ListItem.Root>
-			{children}
-		</TreeItemContext.Provider>
+				<div role="listitem">
+					<ListItem.Root
+						{...rest}
+						data-kiwi-expanded={expanded}
+						data-kiwi-selected={selected}
+						className={cx("🥝-tree-item", className)}
+						style={
+							{
+								...style,
+								"--🥝tree-item-level": level,
+							} as React.CSSProperties
+						}
+						ref={forwardedRef}
+						role={undefined}
+					>
+						{content}
+					</ListItem.Root>
+					{children && <div role="list">{children}</div>}
+				</div>
+			</TreeItemContext.Provider>
+		);
+	},
+);
+DEV: TreeItem.displayName = "Tree.Item";
+
+// ----------------------------------------------------------------------------
+
+interface TreeItemContentProps
+	extends React.ComponentProps<typeof ListItem.Content> {}
+
+const TreeItemContent = React.forwardRef<
+	React.ElementRef<typeof ListItem.Content>,
+	TreeItemContentProps
+>((props, forwardedRef) => {
+	return (
+		<ListItem.Content
+			{...props}
+			className={cx("🥝-tree-item-content", props.className)}
+			ref={forwardedRef}
+		/>
 	);
 });
-DEV: TreeItem.displayName = "Tree.Item";
+DEV: TreeItemContent.displayName = "Tree.Content";
+
+// ----------------------------------------------------------------------------
+
+type IconButtonProps = React.ComponentProps<typeof IconButton>;
+
+interface TreeItemExpanderProps
+	extends Omit<IconButtonProps, "variant" | "label" | "icon"> {
+	label?: IconButtonProps["label"];
+	icon?: IconButtonProps["icon"];
+}
+
+const TreeItemExpander = React.forwardRef<
+	React.ElementRef<typeof IconButton>,
+	TreeItemExpanderProps
+>((props, forwardedRef) => {
+	const context = React.useContext(TreeItemContext);
+	const expanded = context?.expanded;
+	return (
+		<IconButton
+			icon={<TreeChevron />}
+			label="Toggle"
+			aria-expanded={expanded === undefined ? undefined : expanded}
+			{...props}
+			className={cx("🥝-tree-item-expander", props.className)}
+			variant="ghost"
+			labelVariant="visually-hidden"
+			ref={forwardedRef}
+		/>
+	);
+});
+DEV: TreeItemExpander.displayName = "Tree.Expander";
+
+// ----------------------------------------------------------------------------
+
+interface TreeChevronProps extends Omit<Ariakit.RoleProps<"svg">, "children"> {}
+
+const TreeChevron = React.forwardRef<React.ElementRef<"svg">, TreeChevronProps>(
+	(props, forwardedRef) => {
+		return (
+			<Icon
+				{...props}
+				render={
+					<Ariakit.Role.svg
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+						render={props.render}
+					>
+						<path d="M4.146 6.146a.5.5 0 0 1 .708 0L8 9.293l3.146-3.147a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 0 1 0-.708Z" />
+					</Ariakit.Role.svg>
+				}
+				className={cx("🥝-tree-chevron", props.className)}
+				ref={forwardedRef}
+			/>
+		);
+	},
+);
+DEV: TreeChevron.displayName = "TreeChevron";
 
 // ----------------------------------------------------------------------------
 
 const TreeItemContext = React.createContext<
 	| {
 			level: number;
+			expanded?: boolean;
 	  }
 	| undefined
 >(undefined);
 
 // ----------------------------------------------------------------------------
 
-export { Tree as Root, TreeItem as Item };
+export {
+	Tree as Root,
+	TreeItem as Item,
+	TreeItemExpander as Expander,
+	TreeItemContent as Content,
+};

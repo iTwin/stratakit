e1cce981076c93f84f9280b6b65aa180fdfde3f5
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-12-18 17:01:57 +0200

    Remove ComplexTreeItems extracted to a separate branch.

--- apps/test-app/app/sandbox.tsx
+++ apps/test-app/app/sandbox.tsx
@@ -356,159 +356,58 @@ const SandboxTreeContext = React.createContext<{
 function SandboxTree() {
 	const [searchParams] = useSearchParams();
 	const scroll = searchParams.get("scroll");
-	const tree = searchParams.get("tree");
 	const [selected, setSelected] = React.useState<string | undefined>();
 	return (
 		<SandboxTreeContext.Provider value={{ selected, setSelected }}>
 			<Tree.Root scroll={scroll === "both" ? "both" : "vertical"}>
-				{tree === "complex" ? <ComplexTreeItems /> : <IdealTreeItems />}
+				<TreeItem label="Guides">
+					<TreeItem label="Tree">
+						<TreeItem label="Guide 4" />
+						<TreeItem label="Guide 3" />
+						<TreeItem label="Guide 2" />
+						<TreeItem label="Guide 1" lockAction />
+					</TreeItem>
+				</TreeItem>
+				<TreeItem label="Other">
+					<TreeItem label="Object 2">
+						<TreeItem label="Path 3" />
+					</TreeItem>
+					<TreeItem label="Object 1" visibilityAction />
+				</TreeItem>
+				<TreeItem label="Road">
+					<TreeItem label="Parking lot access" />
+					<TreeItem label="Site access" lockAction visibilityAction />
+				</TreeItem>
+				<TreeItem label="Parking lot">
+					<TreeItem label="Parking area">
+						<TreeItem label="Bay point 2" />
+						<TreeItem label="Bay point 1" />
+						<TreeItem label="Space point 1" />
+						<TreeItem label="Path 6" />
+					</TreeItem>
+				</TreeItem>
+				<TreeItem label="Building">
+					<TreeItem label="Building area">
+						<TreeItem label="Path 5" />
+					</TreeItem>
+				</TreeItem>
+				<TreeItem label="Sewer">
+					<TreeItem label="Run off pipe">
+						<TreeItem label="Path 4" />
+					</TreeItem>
+				</TreeItem>
+				<TreeItem label="Project boundary">
+					<TreeItem label="Property area">
+						<TreeItem label="Path 1" />
+					</TreeItem>
+				</TreeItem>
+				<TreeItem label="Map">
+					<TreeItem label="Location">
+						<TreeItem label="Terrain" />
+					</TreeItem>
+				</TreeItem>
 			</Tree.Root>
 		</SandboxTreeContext.Provider>
-	);
-}
-
-function IdealTreeItems() {
-	return (
-		<>
-			<TreeItem label="Guides">
-				<TreeItem label="Tree">
-					<TreeItem label="Guide 4" />
-					<TreeItem label="Guide 3" />
-					<TreeItem label="Guide 2" />
-					<TreeItem label="Guide 1" lockAction />
-				</TreeItem>
-			</TreeItem>
-			<TreeItem label="Other">
-				<TreeItem label="Object 2">
-					<TreeItem label="Path 3" />
-				</TreeItem>
-				<TreeItem label="Object 1" visibilityAction />
-			</TreeItem>
-			<TreeItem label="Road">
-				<TreeItem label="Parking lot access" />
-				<TreeItem label="Site access" lockAction visibilityAction />
-			</TreeItem>
-			<TreeItem label="Parking lot">
-				<TreeItem label="Parking area">
-					<TreeItem label="Bay point 2" />
-					<TreeItem label="Bay point 1" />
-					<TreeItem label="Space point 1" />
-					<TreeItem label="Path 6" />
-				</TreeItem>
-			</TreeItem>
-			<TreeItem label="Building">
-				<TreeItem label="Building area">
-					<TreeItem label="Path 5" />
-				</TreeItem>
-			</TreeItem>
-			<TreeItem label="Sewer">
-				<TreeItem label="Run off pipe">
-					<TreeItem label="Path 4" />
-				</TreeItem>
-			</TreeItem>
-			<TreeItem label="Project boundary">
-				<TreeItem label="Property area">
-					<TreeItem label="Path 1" />
-				</TreeItem>
-			</TreeItem>
-			<TreeItem label="Map">
-				<TreeItem label="Location">
-					<TreeItem label="Terrain" />
-				</TreeItem>
-			</TreeItem>
-		</>
-	);
-}
-
-function ComplexTreeItems() {
-	return (
-		<>
-			<TreeItem label="ITC_Master">
-				<TreeItem label="002_Substation" defaultCollapsed>
-					<TreeItem label="002_Substation_A" />
-				</TreeItem>
-				<TreeItem
-					label="005-BENROAD-00-XX-M3-D-00003.dgn"
-					defaultCollapsed
-					lockAction
-					visibilityAction
-				>
-					<TreeItem label="005-BENROAD-00-XX-M3-D-00003-A" />
-				</TreeItem>
-				<TreeItem
-					label="005-BENROAD-00-XX-M3-D-00005.dgn"
-					defaultCollapsed
-					lockAction
-					visibilityAction
-				>
-					<TreeItem label="005-BENROAD-00-XX-M3-D-00005-A" />
-				</TreeItem>
-				<TreeItem label="005-BENROAD-00-XX-M3-G-00002.dgn" defaultCollapsed>
-					<TreeItem
-						label="005-BENROAD-00-XX-M3-G-00002-A"
-						lockAction
-						visibilityAction
-					/>
-				</TreeItem>
-				<TreeItem label="005-BENROAD-00-XX-M3-G-00003.dgn" defaultCollapsed>
-					<TreeItem label="005-BENROAD-00-XX-M3-G-00003-A" />
-				</TreeItem>
-				<TreeItem label="007-aa_master.dgn">
-					<TreeItem label="A-CLNG-LITE" defaultCollapsed>
-						<TreeItem label="A-CLNG-LITE-A" />
-					</TreeItem>
-					<TreeItem label="A-CLNG-TILE">
-						<TreeItem label="A-DOOR-2D-PLAN">
-							<TreeItem label="P00003 [2-KA62]">
-								<TreeItem label="Cell [2-KA63]">
-									<TreeItem label="Cell [2-KA64]">
-										<TreeItem label="Complex Chain [2-KA6A]" />
-										<TreeItem
-											label="Complex Chain [2-KA6B]"
-											lockAction
-											visibilityAction
-										/>
-										<TreeItem
-											label="Complex Chain [2-KA6C]"
-											lockAction
-											visibilityAction
-										/>
-										<TreeItem
-											label="Complex Chain [2-KA6D]"
-											lockAction
-											visibilityAction
-										/>
-										<TreeItem label="Complex Chain [2-KA6E]" />
-										<TreeItem label="Complex Chain [2-KA6F]" />
-										<TreeItem label="Complex Chain [2-KA6G]" />
-										<TreeItem label="Complex Chain [2-KA6H]" />
-										<TreeItem label="Complex Chain [2-KA61]" />
-										<TreeItem label="Complex Chain [2-KA65]" />
-										<TreeItem label="Complex Chain [2-KA66]" />
-										<TreeItem label="Complex Chain [2-KA67]" />
-										<TreeItem label="Complex Chain [2-KA68]" />
-										<TreeItem label="Complex Chain [2-KA69]" />
-									</TreeItem>
-								</TreeItem>
-							</TreeItem>
-							<TreeItem label="P00003 [2-KA74]" defaultCollapsed>
-								<TreeItem label="P00003 [2-KA74-A]" />
-							</TreeItem>
-							<TreeItem label="P00003 [2-KA86]" defaultCollapsed>
-								<TreeItem label="P00003 [2-KA74-A]" />
-							</TreeItem>
-							<TreeItem label="P00003 [2-KA98]" defaultCollapsed>
-								<TreeItem label="P00003 [2-KA98-A]" />
-							</TreeItem>
-							<TreeItem label="P00003 [2-KAAA]" defaultCollapsed>
-								<TreeItem label="P00003 [2-KAAA-A]" />
-							</TreeItem>
-						</TreeItem>
-					</TreeItem>
-				</TreeItem>
-			</TreeItem>
-			<TreeItem label="ITC_Main" lockAction visibilityAction />
-		</>
 	);
 }
 

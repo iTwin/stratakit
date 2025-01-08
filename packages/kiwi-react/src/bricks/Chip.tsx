5808f16f63ec5b0f5d330dea9e67993c9e58e45e
Richard Hill <richard.hill@bentley.com>
2025-01-08 17:59:02 +0000

    Buidling the component definition basing it off Ariakit Role Div

--- packages/kiwi-react/src/bricks/Chip.tsx
+++ packages/kiwi-react/src/bricks/Chip.tsx
@@ -2,9 +2,15 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+import cx from "classnames";
+import * as Ariakit from "@ariakit/react";
 import { forwardRef, type BaseProps } from "./~utils.js";
 
 interface ChipProps extends BaseProps<"div"> {
+	/**
+	 * The variant style of the Chip.
+	 * Use "solid" for primary states and "outline" for less prominent states.
+	 */
 	/** @default "solid" */
 	variant?: "solid" | "outline";
 }
@@ -12,6 +18,17 @@ interface ChipProps extends BaseProps<"div"> {
 /**
  */
 export const Chip = forwardRef<"div", ChipProps>((props, forwardedRef) => {
-	return <div>Chip</div>;
+	const { variant = "solid", children, ...rest } = props;
+
+	return (
+		<Ariakit.Role.div
+			data-kiwi-variant={variant}
+			{...rest}
+			className={cx("🥝-chip", props.className)}
+			ref={forwardedRef}
+		>
+			{children}
+		</Ariakit.Role.div>
+	);
 });
 DEV: Chip.displayName = "Chip";

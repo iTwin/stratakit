dfef672feffeb04bffb45e71eac991bb5f6aafbe
Richard Hill <richard.hill@bentley.com>
2025-01-13 17:37:22 +0000

    Adding in visual tests and removing some styles that aren't required yet.

--- apps/test-app/app/tests/chip/index.spec.ts
+++ apps/test-app/app/tests/chip/index.spec.ts
@@ -6,6 +6,18 @@ import { test, expect } from "#playwright";
 import AxeBuilder from "@axe-core/playwright";
 
 test.describe("@a11y", () => {
+	test.describe("@visual", () => {
+		test("chip default", async ({ page }) => {
+			await page.goto("/tests/chip");
+			await expect(page.locator("body")).toHaveScreenshot();
+		});
+
+		test("chips visual vairants", async ({ page }) => {
+			await page.goto("/tests/chip?visual=true");
+			await expect(page.locator("body")).toHaveScreenshot();
+		});
+	});
+
 	test("Axe Page Scan", async ({ page }) => {
 		await page.goto("/tests/chip");
 

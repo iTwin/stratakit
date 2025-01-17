97c0c3e3921534df57b3e85579ad9ddc410cd85d
Mayank <9084735+mayank99@users.noreply.github.com>
2025-01-17 16:50:40 -0500

    implement treeview pattern

--- apps/test-app/app/tests/tree/index.spec.ts
+++ apps/test-app/app/tests/tree/index.spec.ts
@@ -8,34 +8,29 @@ import AxeBuilder from "@axe-core/playwright";
 test("default", async ({ page }) => {
 	await page.goto("/tests/tree");
 
-	const tree = page.getByRole("list").first();
+	const tree = page.getByRole("tree").first();
 	await expect(tree).toBeVisible();
 
-	const items = page.getByRole("listitem");
+	const items = page.getByRole("treeitem");
 	await expect(items).toHaveCount(7);
-
-	const expander = page.getByRole("button", { name: "Toggle" });
 
 	const item1 = items.filter({
 		has: page.getByText("Item 1", { exact: true }),
 	});
 	await expect(item1).toBeVisible();
-	await expect(item1.locator(expander)).toHaveAttribute(
-		"aria-expanded",
-		"true",
-	);
+	await expect(item1).toHaveAttribute("aria-expanded", "true");
 
-	const item1_1 = item1.getByRole("listitem").filter({
+	const item1_1 = item1.getByRole("treeitem").filter({
 		has: page.getByText("Item 1.1"),
 	});
 	await expect(item1_1).toBeVisible();
-	await expect(item1_1.locator(expander)).not.toBeVisible();
+	await expect(item1_1).not.toHaveAttribute("aria-expanded");
 });
 
 test.describe("@visual", () => {
 	test("default", async ({ page }) => {
 		await page.goto("/tests/tree");
-		const tree = page.getByRole("list").first();
+		const tree = page.getByRole("tree").first();
 		await expect(tree).toBeVisible();
 		await expect(page.locator("body")).toHaveScreenshot();
 	});
@@ -43,7 +38,7 @@ test.describe("@visual", () => {
 	test("hovered item", async ({ page }) => {
 		await page.goto("/tests/tree");
 
-		const item = page.getByRole("button", { name: "Item 1.2" });
+		const item = page.getByRole("treeitem", { name: "Item 1.2" });
 		item.hover();
 
 		await expect(page.locator("body")).toHaveScreenshot();
@@ -52,7 +47,7 @@ test.describe("@visual", () => {
 	test("focused item", async ({ page }) => {
 		await page.goto("/tests/tree");
 
-		const item = page.getByRole("button", { name: "Item 1.2" });
+		const item = page.getByRole("treeitem", { name: "Item 1.2" });
 		item.focus();
 
 		await expect(page.locator("body")).toHaveScreenshot();
@@ -60,14 +55,14 @@ test.describe("@visual", () => {
 
 	test("overflow", async ({ page }) => {
 		await page.goto("/tests/tree?overflow");
-		const tree = page.getByRole("list").first();
+		const tree = page.getByRole("tree").first();
 		await expect(tree).toBeVisible();
 		await expect(page.locator("body")).toHaveScreenshot();
 	});
 
 	test("selected", async ({ page }) => {
 		await page.goto("/tests/tree?selected");
-		const tree = page.getByRole("list").first();
+		const tree = page.getByRole("tree").first();
 		await expect(tree).toBeVisible();
 		await expect(page.locator("body")).toHaveScreenshot();
 	});
@@ -77,7 +72,7 @@ test.describe("@a11y", () => {
 	test("Axe Page Scan", async ({ page }) => {
 		await page.goto("/tests/tree");
 
-		const tree = page.getByRole("list").first();
+		const tree = page.getByRole("tree").first();
 		await expect(tree).toBeVisible();
 
 		const axe = new AxeBuilder({ page });

1abb3adc3529a63e8652dce48e25a23b8ddd347e
Uyen Doan <uyen.doan@bentley.com>
2025-01-21 17:18:11 -0600

    added commnet

--- packages/kiwi-react/src/bricks/Table.tsx
+++ packages/kiwi-react/src/bricks/Table.tsx
@@ -11,6 +11,35 @@ import { forwardRef, type BaseProps } from "./~utils.js";
 
 interface TableProps extends BaseProps {}
 
+/**
+ * A table is a grid of rows and columns that displays data in a structured format.
+ *
+ * `Table.Root` is the root component for a table.
+ * `Table.Header`, `Table.Body`, and `Table.Cell` can be nested inside a `Table.Root` to create a table structure.
+ *
+ * Example:
+ * ```tsx
+ * <Table.Root>
+ * 	<Table.Header>
+ * 		<Table.Row>
+ * 			<Table.Cell>Header 1</Table.Cell>
+ * 			<Table.Cell>Header 2</Table.Cell>
+ * 		</Table.Row>
+ * 	</Table.Header>
+ *
+ * 	<Table.Body>
+ * 		<Table.Row>
+ * 			<Table.Cell>Cell 1.1</Table.Cell>
+ * 			<Table.Cell>Cell 1.2</Table.Cell>
+ * 		</Table.Row>
+ * 		<Table.Row>
+ * 			<Table.Cell>Cell 2.1</Table.Cell>
+ * 			<Table.Cell>Cell 2.2</Table.Cell>
+ * 		</Table.Row>
+ * 	</Table.Body>
+ * </Table.Root>
+ * ```
+ */
 const Table = forwardRef<"div", TableProps>((props, forwardedRef) => {
 	return (
 		<Ariakit.Role.div
@@ -28,6 +57,20 @@ DEV: Table.displayName = "Table.Root";
 
 interface TableHeaderProps extends BaseProps {}
 
+/**
+ * `Table.Header` is a column component of cells that labels the columns of a table.
+ * `Table.Row` and `Table.Cell` can be nested inside a `Table.Header` to create a header row.
+ *
+ * Example:
+ * ```tsx
+ *	<Table.Header>
+ *		<Table.Row>
+ *			<Table.Cell>Header 1</Table.Cell>
+ * 			<Table.Cell>Header 2</Table.Cell>
+ *		</Table.Row>
+ *	</Table.Header>
+ * ```
+ */
 const TableHeader = forwardRef<"div", TableHeaderProps>(
 	(props, forwardedRef) => {
 		return (
@@ -49,6 +92,24 @@ DEV: TableHeader.displayName = "Table.Header";
 
 interface TableBodyProps extends BaseProps {}
 
+/**
+ * `Table.Body` is a component that contains the rows of table data.
+ * Multiple `Table.Row`s and `Table.Cell`s can be nested inside a `Table.Body` to create a table body.
+ *
+ * Example:
+ * ```tsx
+ *	<Table.Body>
+ *		<Table.Row>
+ *			<Table.Cell>Cell 1.1</Table.Cell>
+ *			<Table.Cell>Cell 1.2</Table.Cell>
+ *		</Table.Row>
+ *		<Table.Row>
+ *			<Table.Cell>Cell 2.1</Table.Cell>
+ *			<Table.Cell>Cell 2.2</Table.Cell>
+ *		</Table.Row>
+ *	</Table.Body>
+ * ```
+ */
 const TableBody = forwardRef<"div", TableBodyProps>((props, forwardedRef) => {
 	return (
 		<TableVariantContext.Provider value={{ variant: "body" }}>
@@ -81,6 +142,17 @@ interface TableRowProps extends BaseProps {
 	disabled?: boolean;
 }
 
+/**
+ * `Table.Row` is a component that contains the cells of a table row.
+ *
+ * Example:
+ * ```tsx
+ *	<Table.Row>
+ *		<Table.Cell>Cell 1.1</Table.Cell>
+ *		<Table.Cell>Cell 1.2</Table.Cell>
+ *	</Table.Row>
+ * ```
+ */
 const TableRow = forwardRef<"div", TableRowProps>((props, forwardedRef) => {
 	const { children, selected, disabled, ...rest } = props;
 	const tableContext = React.useContext(TableVariantContext);
@@ -105,6 +177,14 @@ DEV: TableRow.displayName = "Table.Row";
 
 interface TableCellProps extends BaseProps {}
 
+/**
+ * `Table.Cell` is a component that contains the data of a table cell.
+ *
+ * Example:
+ * ```tsx
+ *	<Table.Cell>Cell 1.1</Table.Cell>
+ * ```
+ */
 const TableCell = forwardRef<"span", TableCellProps>((props, forwardedRef) => {
 	const tableContext = React.useContext(TableVariantContext);
 	const isHeaderCell = tableContext?.variant === "header";

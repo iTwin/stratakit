e98c91882fc61e5453da23066323e80f4b3586c8
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-01-23 17:29:35 -0500

    Visual test

--- apps/test-app/app/tests/select/index.tsx
+++ apps/test-app/app/tests/select/index.tsx
@@ -7,17 +7,59 @@ import { definePage } from "~/~utils.tsx";
 
 export const handle = { title: "Select" };
 
-export default definePage(function Page() {
+export default definePage(
+	function Page() {
+		return (
+			<Field>
+				<Label>Fruit</Label>
+				<Select.Root>
+					<Select.HtmlSelect>
+						<option value="apple">Apple</option>
+						<option value="orange">Orange</option>
+						<option value="kiwi">Kiwi</option>
+					</Select.HtmlSelect>
+				</Select.Root>
+			</Field>
+		);
+	},
+	{ visual: VisualTest },
+);
+
+function VisualTest() {
 	return (
-		<Field>
-			<Label>Fruit</Label>
-			<Select.Root>
-				<Select.HtmlSelect>
-					<option value="apple">Apple</option>
-					<option value="orange">Orange</option>
-					<option value="kiwi">Kiwi</option>
-				</Select.HtmlSelect>
-			</Select.Root>
-		</Field>
+		<div style={{ display: "grid", gap: 4 }}>
+			<Field>
+				<Label>Fruit</Label>
+				<Select.Root>
+					<Select.HtmlSelect variant="solid">
+						<option value="apple">Apple</option>
+						<option value="orange">Orange</option>
+						<option value="kiwi">Kiwi</option>
+					</Select.HtmlSelect>
+				</Select.Root>
+			</Field>
+
+			<Field>
+				<Label>Fruit</Label>
+				<Select.Root>
+					<Select.HtmlSelect variant="outline">
+						<option value="apple">Apple</option>
+						<option value="orange">Orange</option>
+						<option value="kiwi">Kiwi</option>
+					</Select.HtmlSelect>
+				</Select.Root>
+			</Field>
+
+			<Field>
+				<Label>Fruit</Label>
+				<Select.Root>
+					<Select.HtmlSelect variant="ghost">
+						<option value="apple">Apple</option>
+						<option value="orange">Orange</option>
+						<option value="kiwi">Kiwi</option>
+					</Select.HtmlSelect>
+				</Select.Root>
+			</Field>
+		</div>
 	);
-});
+}

3de3c374e3fa816692ad66b23550fd9cdece30f2
Uyen Doan <uyen.doan@bentley.com>
2025-01-28 14:22:16 -0600

    added simple visual tests

---
+++ apps/test-app/app/tests/table/index.spec.ts
@@ -0,0 +1,56 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+
+test("default", async ({ page }) => {
+	await page.goto("/tests/table");
+
+	const table = page.getByRole("table").first();
+	await expect(table).toBeVisible();
+
+	const header = page.getByRole("columnheader").first();
+	await expect(header).toBeVisible();
+
+	const rowGroups = page.getByRole("rowgroup").first();
+	await expect(rowGroups).toBeVisible();
+
+	const rows = page.getByRole("row").first();
+	await expect(rows).toBeVisible();
+});
+
+test.describe("@visual", () => {
+	test("default", async ({ page }) => {
+		await page.goto("/tests/table?visual");
+		const table = page.getByRole("table").first();
+		await expect(table).toBeVisible();
+		await expect(table).toHaveScreenshot();
+	});
+
+	test("header", async ({ page }) => {
+		await page.goto("/tests/table");
+		const columnHeaders = page.getByRole("columnheader");
+		await expect(columnHeaders).toHaveCount(3);
+	});
+
+	test("table rows", async ({ page }) => {
+		await page.goto("/tests/table");
+		const rows = page.getByRole("row");
+		await expect(rows).toHaveCount(11);
+	});
+});
+
+test.describe("@a11y", () => {
+	test("Axe Page Scan", async ({ page }) => {
+		await page.goto("/tests/table");
+
+		const table = page.getByRole("table");
+		await expect(table).toBeVisible();
+
+		const axe = new AxeBuilder({ page });
+		const accessibilityScan = await axe.analyze();
+		expect(accessibilityScan.violations).toEqual([]);
+	});
+});
--- apps/test-app/app/tests/table/index.tsx
+++ apps/test-app/app/tests/table/index.tsx
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 import { definePage } from "~/~utils.tsx";
 import * as Table from "@itwin/itwinui-react-internal/src/bricks/Table.tsx";
-import { DropdownMenu } from "@itwin/itwinui-react/bricks";
 import * as React from "react";
 
 export const handle = { title: "Table" };
@@ -15,50 +14,17 @@ export default definePage(
 			return string.charAt(0).toUpperCase() + string.slice(1);
 		};
 
-		const statusMenu = (
-			<DropdownMenu.Root>
-				<DropdownMenu.Button>Status</DropdownMenu.Button>
-				<DropdownMenu.Content>
-					<DropdownMenu.Item>New</DropdownMenu.Item>
-					<DropdownMenu.Item>Done</DropdownMenu.Item>
-					<DropdownMenu.Item>In Progress</DropdownMenu.Item>
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
-		);
+		const generateData = React.useCallback((numberOfData: number) => {
+			return Array(numberOfData)
+				.fill(null)
+				.map((_, index) => ({
+					id: index,
+					name: `Name ${index}`,
+					description: `Description ${index}`,
+				}));
+		}, []);
 
-		const data = React.useMemo(
-			() => [
-				{
-					id: 1,
-					name: "Name 1",
-					description: "Description 1",
-					date: "01-01-2025",
-					status: statusMenu,
-				},
-				{
-					id: 2,
-					name: "Name 2",
-					description: "Description 2",
-					date: "01-02-2025",
-					status: statusMenu,
-				},
-				{
-					id: 3,
-					name: "Name 3",
-					description: "Description 3",
-					date: "01-02-2025",
-					status: statusMenu,
-				},
-				{
-					id: 4,
-					name: "Name 4",
-					description: "Description 4",
-					date: "01-04-2025",
-					status: statusMenu,
-				},
-			],
-			[],
-		);
+		const data = React.useMemo(() => generateData(10), [generateData]);
 
 		return (
 			<Table.Root>
@@ -87,7 +53,59 @@ export default definePage(
 );
 
 function VisualTest() {
-	const id = React.useId();
+	const capitalizeFirstLetter = (string: string) => {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	};
+	const data = React.useMemo(
+		() => [
+			{
+				id: 1,
+				name: "Name 1",
+				description: "Description 1",
+				date: "01-01-2025",
+			},
+			{
+				id: 2,
+				name: "Name 2",
+				description: "Description 2",
+				date: "01-02-2025",
+			},
+			{
+				id: 3,
+				name: "Name 3",
+				description: "Description 3",
+				date: "01-02-2025",
+			},
+			{
+				id: 4,
+				name: "Name 4",
+				description: "Description 4",
+				date: "01-04-2025",
+			},
+		],
+		[],
+	);
 
-	return <></>;
+	return (
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					{Object.keys(data[0]).map((columnName: string) => (
+						<Table.Cell key={columnName}>
+							{capitalizeFirstLetter(columnName)}
+						</Table.Cell>
+					))}
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{data.map((row) => (
+					<Table.Row key={row.id}>
+						{Object.values(row).map((value) => (
+							<Table.Cell key={row.id}>{value}</Table.Cell>
+						))}
+					</Table.Row>
+				))}
+			</Table.Body>
+		</Table.Root>
+	);
 }

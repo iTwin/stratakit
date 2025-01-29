51112a9a72fcccd918b96961167541a5bf243de4
Uyen Doan <uyen.doan@bentley.com>
2025-01-29 08:57:35 -0600

    removed JS loop in exmaple

--- apps/test-app/app/tests/table/index.tsx
+++ apps/test-app/app/tests/table/index.tsx
@@ -10,81 +10,85 @@ export const handle = { title: "Table" };
 
 export default definePage(
 	function Page() {
-		const capitalizeFirstLetter = (string: string) => {
-			return string.charAt(0).toUpperCase() + string.slice(1);
-		};
-
-		const generateData = React.useCallback((numberOfData: number) => {
-			return Array(numberOfData)
-				.fill(null)
-				.map((_, index) => ({
-					id: index,
-					name: `Name ${index}`,
-					description: `Description ${index}`,
-				}));
-		}, []);
-
-		const data = React.useMemo(() => generateData(10), [generateData]);
-
 		return (
 			<Table.Root>
 				<Table.Header>
 					<Table.Row>
-						{Object.keys(data[0]).map((columnName: string) => (
-							<Table.Cell key={columnName}>
-								{capitalizeFirstLetter(columnName)}
-							</Table.Cell>
-						))}
+						<Table.Cell>Name</Table.Cell>
+						<Table.Cell>Description</Table.Cell>
 					</Table.Row>
 				</Table.Header>
 				<Table.Body>
-					{data.map((row) => (
-						<Table.Row key={row.id}>
-							{Object.values(row).map((value) => (
-								<Table.Cell key={row.id}>{value}</Table.Cell>
-							))}
-						</Table.Row>
-					))}
+					<Table.Row>
+						<Table.Cell>Name 1</Table.Cell>
+						<Table.Cell>Description 1</Table.Cell>
+					</Table.Row>
+					<Table.Row>
+						<Table.Cell>Name 2</Table.Cell>
+						<Table.Cell>Description 2</Table.Cell>
+					</Table.Row>
+					<Table.Row>
+						<Table.Cell>Name 3</Table.Cell>
+						<Table.Cell>Description 3</Table.Cell>
+					</Table.Row>
+					<Table.Row>
+						<Table.Cell>Name 4</Table.Cell>
+						<Table.Cell>Description 4</Table.Cell>
+					</Table.Row>
 				</Table.Body>
 			</Table.Root>
 		);
 	},
-	{ visual: VisualTest },
+	{ visual: VisualTest, scroll: ScrollTest },
 );
 
 function VisualTest() {
+	return (
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					<Table.Cell>Name</Table.Cell>
+					<Table.Cell>Description</Table.Cell>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				<Table.Row>
+					<Table.Cell>Name 1</Table.Cell>
+					<Table.Cell>Description 1</Table.Cell>
+				</Table.Row>
+				<Table.Row>
+					<Table.Cell>Name 2</Table.Cell>
+					<Table.Cell>Description 2</Table.Cell>
+				</Table.Row>
+				<Table.Row>
+					<Table.Cell>Name 3</Table.Cell>
+					<Table.Cell>Description 3</Table.Cell>
+				</Table.Row>
+				<Table.Row>
+					<Table.Cell>Name 4</Table.Cell>
+					<Table.Cell>Description 4</Table.Cell>
+				</Table.Row>
+			</Table.Body>
+		</Table.Root>
+	);
+}
+
+function ScrollTest() {
 	const capitalizeFirstLetter = (string: string) => {
 		return string.charAt(0).toUpperCase() + string.slice(1);
 	};
-	const data = React.useMemo(
-		() => [
-			{
-				id: 1,
-				name: "Name 1",
-				description: "Description 1",
-				date: "01-01-2025",
-			},
-			{
-				id: 2,
-				name: "Name 2",
-				description: "Description 2",
-				date: "01-02-2025",
-			},
-			{
-				id: 3,
-				name: "Name 3",
-				description: "Description 3",
-				date: "01-02-2025",
-			},
-			{
-				id: 4,
-				name: "Name 4",
-				description: "Description 4",
-				date: "01-04-2025",
-			},
-		],
-		[],
-	);
+
+	const generateData = React.useCallback((numberOfData: number) => {
+		return Array(numberOfData)
+			.fill(null)
+			.map((_, index) => ({
+				id: index,
+				name: `Name ${index}`,
+				description: `Description ${index}`,
+			}));
+	}, []);
+
+	const data = React.useMemo(() => generateData(10), [generateData]);
 
 	return (
 		<Table.Root>

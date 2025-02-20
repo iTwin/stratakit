4bd85bd88015ea38e4790ac3bd7999cca4ea4f02
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2025-02-19 22:01:40 -0500

    Add `<Badge>` component (#360)
    Co-authored-by: GerardasB <10091419+GerardasB@users.noreply.github.com>

---
+++ packages/kiwi-react/src/bricks/Badge.tsx
@@ -0,0 +1,52 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import cx from "classnames";
+import * as Ariakit from "@ariakit/react";
+import { forwardRef, type BaseProps } from "./~utils.js";
+
+interface BadgeProps extends Omit<BaseProps<"span">, "children"> {
+	/**
+	 * The label displayed inside the badge.
+	 */
+	label: string;
+
+	/**
+	 * The tone of the badge.
+	 * @default "neutral"
+	 */
+	tone?: "neutral" | "info" | "positive" | "attention" | "critical";
+
+	/**
+	 * The variant style of the badge.
+	 * @default "solid"
+	 */
+	variant?: "solid" | "muted" | "outline";
+}
+
+/**
+ * A badge component, typically used to designate the status of an item.
+ *
+ * Example:
+ * ```tsx
+ * <Badge label="Value" />
+ * <Badge label="Value" tone="info" variant="outline" />
+ * ```
+ */
+export const Badge = forwardRef<"span", BadgeProps>((props, forwardedRef) => {
+	const { tone = "neutral", variant = "solid", label, ...rest } = props;
+
+	return (
+		<Ariakit.Role.span
+			{...rest}
+			data-kiwi-tone={tone}
+			data-kiwi-variant={variant}
+			className={cx("🥝-badge", props.className)}
+			ref={forwardedRef}
+		>
+			{label}
+		</Ariakit.Role.span>
+	);
+});
+DEV: Badge.displayName = "Badge";

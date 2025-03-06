7e3fa35d062876b6dd6a2e968c608dc6fdcf29b5
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-03-06 17:54:05 -0500

    Remove test

--- apps/test-app/app/tests/skeleton/index.spec.ts
+++ apps/test-app/app/tests/skeleton/index.spec.ts
@@ -5,28 +5,9 @@
 import { test, expect } from "#playwright";
 import AxeBuilder from "@axe-core/playwright";
 
-const sizes = ["xsmall", "small", "medium", "large", "xlarge"] as const;
-
-test.describe("default", () => {
-	test("default", async ({ page }) => {
-		for (const size of sizes) {
-			for (const variant of ["text", "object"]) {
-				await page.goto(`/tests/skeleton?variant=${variant}&size=${size}`);
-
-				const skeleton = page.locator(".🥝-skeleton");
-
-				await expect(skeleton).toHaveAttribute("data-kiwi-variant", variant);
-				await expect(skeleton).toHaveAttribute("data-kiwi-size", size);
-			}
-		}
-	});
-});
-
-test.describe("@visual", () => {
-	test("default", async ({ page }) => {
-		await page.goto("/tests/skeleton?visual");
-		await expect(page.locator("body")).toHaveScreenshot();
-	});
+test("@visual", async ({ page }) => {
+	await page.goto("/tests/skeleton?visual");
+	await expect(page.locator("body")).toHaveScreenshot();
 });
 
 test.describe("@a11y", () => {

730e716487722a5e1003cd7273d009e1e23d4c9f
Nathan Knowler <nathan@knowler.dev>
2025-03-28 14:17:45 -0500

    Add `AccordionItem` component

---
+++ apps/test-app/app/tests/accordion-item/index.spec.ts
@@ -0,0 +1,90 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import AxeBuilder from "@axe-core/playwright";
+import { expect, test } from "#playwright";
+
+test("default", async ({ page }) => {
+	await page.goto("/tests/accordion-item");
+
+	const trigger = page.getByTestId("trigger");
+	const content = page.getByTestId("content");
+
+	await expect(trigger).toMatchAriaSnapshot(`
+		- button "Label" [expanded=false]
+	`);
+	await expect(content).not.toBeVisible();
+
+	trigger.click();
+
+	await expect(trigger).toMatchAriaSnapshot(`
+		- button "Label" [expanded=true]
+	`);
+	await expect(content).toBeVisible();
+});
+
+test.describe("@visual", () => {
+	test("default", async ({ page }) => {
+		await page.goto("/tests/accordion-item?visual");
+		await expect(page.locator("body")).toHaveScreenshot();
+	});
+
+	test("hovered collapsed item", async ({ page }) => {
+		await page.goto("/tests/accordion-item");
+
+		const item = page.getByRole("button", { name: "Label" });
+		item.hover();
+
+		await expect(page.locator("body")).toHaveScreenshot();
+	});
+
+	test("hovered expanded item", async ({ page }) => {
+		await page.goto("/tests/accordion-item?defaultOpen");
+
+		const item = page.getByRole("button", { name: "Label" });
+		item.hover();
+
+		await expect(page.locator("body")).toHaveScreenshot();
+	});
+
+	test("focused collapsed item", async ({ page }) => {
+		await page.goto("/tests/accordion-item");
+
+		const item = page.getByRole("button", { name: "Label" });
+		item.focus();
+
+		await expect(page.locator("body")).toHaveScreenshot();
+	});
+
+	test("focused expanded item", async ({ page }) => {
+		await page.goto("/tests/accordion-item?defaultOpen");
+
+		const item = page.getByRole("button", { name: "Label" });
+		item.focus();
+
+		await expect(page.locator("body")).toHaveScreenshot();
+	});
+
+	/*
+	test("forced-colors", async ({ page, browserName }) => {
+		test.skip(
+			browserName === "webkit",
+			"Webkit does not support forced-colors",
+		);
+		await page.goto("/tests/accordion?visual");
+		await page.emulateMedia({ forcedColors: "active" });
+		await expect(page.locator("body")).toHaveScreenshot();
+	});
+  */
+});
+
+test.describe("@a11y", () => {
+	test("Axe Page Scan", async ({ page }) => {
+		await page.goto("/tests/accordion-item");
+
+		const axe = new AxeBuilder({ page });
+		const accessibilityScan = await axe.analyze();
+		expect(accessibilityScan.violations).toEqual([]);
+	});
+});
---
+++ apps/test-app/app/tests/accordion-item/index.tsx
@@ -0,0 +1,79 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import {
+	unstable_AccordionItem as AccordionItem,
+	Icon,
+} from "@stratakit/bricks";
+import placeholderIcon from "@stratakit/icons/placeholder.svg";
+import { definePage } from "~/~utils.tsx";
+
+export default definePage(
+	function Page({
+		label = "Label",
+		content = "Body",
+		withDecoration,
+		defaultOpen: defaultOpenProp,
+	}) {
+		const defaultOpen = Boolean(defaultOpenProp);
+		return (
+			<>
+				<AccordionItem.Root defaultOpen={defaultOpen}>
+					<AccordionItem.Trigger data-testid="trigger">
+						{withDecoration ? (
+							<AccordionItem.Decoration
+								render={<Icon href={placeholderIcon} />}
+							/>
+						) : null}
+						<AccordionItem.Label>{label}</AccordionItem.Label>
+						<AccordionItem.Marker />
+					</AccordionItem.Trigger>
+					<AccordionItem.Content data-testid="content">
+						{content}
+					</AccordionItem.Content>
+				</AccordionItem.Root>
+			</>
+		);
+	},
+	{
+		visual: VisualTest,
+	},
+);
+
+export function VisualTest() {
+	return (
+		<>
+			<AccordionItem.Root>
+				<AccordionItem.Trigger>
+					<AccordionItem.Label>Label</AccordionItem.Label>
+					<AccordionItem.Marker />
+				</AccordionItem.Trigger>
+				<AccordionItem.Content>Body</AccordionItem.Content>
+			</AccordionItem.Root>
+			<AccordionItem.Root defaultOpen>
+				<AccordionItem.Trigger>
+					<AccordionItem.Label>Label</AccordionItem.Label>
+					<AccordionItem.Marker />
+				</AccordionItem.Trigger>
+				<AccordionItem.Content>Body</AccordionItem.Content>
+			</AccordionItem.Root>
+			<AccordionItem.Root>
+				<AccordionItem.Trigger>
+					<AccordionItem.Decoration render={<Icon href={placeholderIcon} />} />
+					<AccordionItem.Label>Label</AccordionItem.Label>
+					<AccordionItem.Marker />
+				</AccordionItem.Trigger>
+				<AccordionItem.Content>Body</AccordionItem.Content>
+			</AccordionItem.Root>
+			<AccordionItem.Root defaultOpen>
+				<AccordionItem.Trigger>
+					<AccordionItem.Decoration render={<Icon href={placeholderIcon} />} />
+					<AccordionItem.Label>Label</AccordionItem.Label>
+					<AccordionItem.Marker />
+				</AccordionItem.Trigger>
+				<AccordionItem.Content>Body</AccordionItem.Content>
+			</AccordionItem.Root>
+		</>
+	);
+}

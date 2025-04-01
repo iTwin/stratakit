ebda5af2e624c042f76378c49ab2fce4d43f139b
Mayank <9084735+mayank99@users.noreply.github.com>
2025-04-01 16:32:24 -0400

    visual test

--- apps/test-app/app/tests/toolbar/index.spec.ts
+++ apps/test-app/app/tests/toolbar/index.spec.ts
@@ -38,6 +38,23 @@ test("keyboard navigation", async ({ page }) => {
 	await expect(items.first()).toBeFocused();
 });
 
+test.describe("@visual", () => {
+	test("default", async ({ page }) => {
+		await page.goto("/tests/toolbar?visual=true");
+		await expect(page.locator("body")).toHaveScreenshot();
+	});
+
+	test("forced-colors", async ({ page, browserName }) => {
+		test.skip(
+			browserName === "webkit",
+			"Webkit does not support forced-colors",
+		);
+		await page.goto("/tests/toolbar?visual=true");
+		await page.emulateMedia({ forcedColors: "active" });
+		await expect(page.locator("body")).toHaveScreenshot();
+	});
+});
+
 test.describe("@a11y", () => {
 	test("Axe Page Scan", async ({ page }) => {
 		await page.goto("/tests/toolbar");

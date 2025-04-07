550563cf2489366d196a726a92c1b75dd6156674
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-04-07 17:51:53 +0300

    Remove dismiss button

--- packages/kiwi-react/src/bricks/ErrorRegion.tsx
+++ packages/kiwi-react/src/bricks/ErrorRegion.tsx
@@ -11,9 +11,8 @@ import {
 } from "@ariakit/react/dialog";
 import { Role } from "@ariakit/react/role";
 import { forwardRef, type BaseProps } from "./~utils.js";
-import { ChevronDown, Dismiss, StatusWarning } from "./Icon.js";
+import { ChevronDown, StatusWarning } from "./Icon.js";
 import { Text } from "./Text.js";
-import { IconButton } from "./IconButton.js";
 import { Button } from "./Button.js";
 import { useControlledState } from "./~hooks.js";
 import { VisuallyHidden } from "./VisuallyHidden.js";
@@ -145,10 +144,6 @@ interface ErrorRegionItemProps extends Omit<BaseProps, "children"> {
 	 * The actions available for this item. Must be a list of anchors, each rendered as a button using `<Anchor render={<button />} />`.
 	 */
 	actions?: React.ReactNode;
-	/**
-	 * Callback fired when the error item is dismissed.
-	 */
-	onDismiss?: () => void;
 }
 
 /**
@@ -180,11 +175,8 @@ const ErrorRegionItem = forwardRef<"div", ErrorRegionItemProps>(
 			message,
 			messageId = `${generatedId}-message`,
 			actions,
-			onDismiss,
 			...rest
 		} = props;
-
-		const dismissButtonId = `${generatedId}-dismiss`;
 
 		return (
 			<Role.div
@@ -200,17 +192,6 @@ const ErrorRegionItem = forwardRef<"div", ErrorRegionItemProps>(
 				>
 					{message}
 				</Text>
-				{onDismiss && (
-					<IconButton
-						id={dismissButtonId}
-						className="🥝-error-region-item-dismiss"
-						variant="ghost"
-						label="Dismiss"
-						aria-labelledby={`${dismissButtonId} ${messageId}`}
-						icon={<Dismiss />}
-						onClick={onDismiss}
-					/>
-				)}
 				<div className="🥝-error-region-item-actions">{actions}</div>
 			</Role.div>
 		);

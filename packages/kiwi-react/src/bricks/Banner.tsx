a9a35f592175fa523474b27ffcc4ce172a7b0c15
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-04-14 17:44:36 -0400

    Code quality (import order) fix

--- packages/kiwi-react/src/bricks/Banner.tsx
+++ packages/kiwi-react/src/bricks/Banner.tsx
@@ -2,6 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+
 import { Role } from "@ariakit/react/role";
 import cx from "classnames";
 import * as React from "react";

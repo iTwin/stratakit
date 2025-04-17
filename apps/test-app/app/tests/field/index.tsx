53482808261c0b1ed4e5da8c91aa9ab0152c0128
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2025-04-17 12:01:44 -0400

    Fix hover of textarea within field demo page.

--- apps/test-app/app/tests/field/index.tsx
+++ apps/test-app/app/tests/field/index.tsx
@@ -66,7 +66,7 @@ export default definePage(
 
 function VisualTestForTextlikeControls() {
 	return (
-		<div style={{ display: "grid", gap: 16 }}>
+		<div style={{ display: "grid", gap: 16, justifyItems: "start" }}>
 			{/* Default layout for text controls (block) */}
 			<Field.Root>
 				<Field.Label>Text control</Field.Label>

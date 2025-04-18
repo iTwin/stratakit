791287b5d220c0a87ca96887f8caf9c49cd9c2ca
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-04-18 16:23:28 -0400

    🛫

--- apps/test-app/app/tests/skeleton/index.tsx
+++ apps/test-app/app/tests/skeleton/index.tsx
@@ -3,7 +3,16 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { Skeleton, VisuallyHidden } from "@stratakit/bricks";
+import {
+	Avatar,
+	Button,
+	Field,
+	Select,
+	Skeleton,
+	Text,
+	VisuallyHidden,
+} from "@stratakit/bricks";
+import * as React from "react";
 import { definePage } from "~/~utils.tsx";
 
 export const handle = { title: "Skeleton" };
@@ -25,32 +34,122 @@ export default definePage(
 	{ visual: VisualTest },
 );
 
-function VisualTest() {
+function VisualTest({ controls }: { controls?: boolean }) {
+	const [isStarted, setIsStarted] = React.useState(false);
+	const [count, setCount] = React.useState(1);
+
 	return (
 		<div style={{ display: "flex", flexDirection: "column", rowGap: 4 }}>
-			<div>
+			{controls != null ? (
+				<div>
+					<Button onClick={() => setIsStarted((prev) => !prev)}>Toggle</Button>
+
+					<Field.Root layout="inline">
+						<Field.Label>Count</Field.Label>
+						<Field.Control
+							render={(controlProps) => (
+								<Select.Root>
+									<Select.HtmlSelect
+										{...controlProps}
+										onChange={(e) => setCount(Number(e.target.value))}
+									>
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+									</Select.HtmlSelect>
+								</Select.Root>
+							)}
+						/>
+					</Field.Root>
+				</div>
+			) : null}
+
+			<div style={{ gap: 8, display: "flex", flexDirection: "column" }}>
 				{sizes.map((textSize) => (
-					<Skeleton
+					<div
 						key={textSize}
-						variant="text"
-						size={textSize}
-						style={{ marginBottom: 8 }}
-					/>
+						style={{
+							marginBlockStart: 8,
+							gap: 8,
+							display: "flex",
+						}}
+					>
+						{new Array(count).fill(0).map(() =>
+							isStarted ? (
+								<Skeleton key={textSize} variant="text" size={textSize} />
+							) : (
+								<Text
+									key={textSize}
+									style={{ lineHeight: 1 }}
+									variant={skeletonSizeTextVariantMap[textSize]}
+								>
+									Lorem ipsum
+								</Text>
+							),
+						)}
+					</div>
 				))}
 				<VisuallyHidden>Loading…</VisuallyHidden>
 			</div>
 
-			<div style={{ gap: 16 }}>
+			<div
+				style={{
+					marginBlockStart: 8,
+					gap: 8,
+					display: "flex",
+					flexDirection: "column",
+				}}
+			>
 				{sizes.map((objectSize) => (
-					<Skeleton
+					<div
 						key={objectSize}
-						variant="object"
-						size={objectSize}
-						style={{ marginBottom: 8 }}
-					/>
+						style={{
+							marginBlockStart: 8,
+							gap: 8,
+							display: "flex",
+						}}
+					>
+						{new Array(count)
+							.fill(0)
+							.map(() =>
+								isStarted ? (
+									<Skeleton
+										key={objectSize}
+										variant="object"
+										size={objectSize}
+									/>
+								) : (
+									<CustomAvatar key={objectSize} size={objectSize} />
+								),
+							)}
+					</div>
 				))}
 				<VisuallyHidden>Loading…</VisuallyHidden>
 			</div>
 		</div>
 	);
 }
+
+const CustomAvatar = ({ size }: { size: (typeof sizes)[number] }) => {
+	if (size !== "xsmall") {
+		return <Avatar initials="JD" size={size} />;
+	}
+	return (
+		<Avatar
+			initials="JD"
+			size="small"
+			style={{
+				inlineSize: 12,
+				blockSize: 12,
+			}}
+		/>
+	);
+};
+
+const skeletonSizeTextVariantMap = {
+	xsmall: "body-sm",
+	small: "body-md",
+	medium: "body-lg",
+	large: "headline-sm",
+	xlarge: "headline-md",
+} as const;

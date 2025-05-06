7274baea73a38e1f8ccee526270e20c4feb26436
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-05-06 16:25:36 -0400

    Show text

--- packages/compat/src/Kbd.tsx
+++ packages/compat/src/Kbd.tsx
@@ -13,16 +13,16 @@ import type { PolymorphicForwardRefComponent } from "./~utils.tsx";
 export const Kbd = React.forwardRef((props, forwardedRef) => {
 	const { children, ...rest } = useCompatProps(props);
 
-	const symbol =
-		typeof children === "string"
+	const mapping =
+		typeof children === "string" && children in IuiKbdKeysToSkKbdSymbols
 			? IuiKbdKeysToSkKbdSymbols[
 					children as keyof typeof IuiKbdKeysToSkKbdSymbols
 				]
 			: undefined;
 
 	return (
-		<SkKbd {...rest} symbol={symbol} ref={forwardedRef}>
-			{symbol == null ? children : undefined}
+		<SkKbd {...rest} symbol={mapping?.symbol} ref={forwardedRef}>
+			{mapping != null ? mapping.text : children}
 		</SkKbd>
 	);
 }) as PolymorphicForwardRefComponent<"a">;
@@ -31,23 +31,26 @@ DEV: Kbd.displayName = "Kbd";
 export { IuiKbdKeys as KbdKeys };
 
 /**
- * Maps IuiKbdKeysValues to SkKbd symbols, if available. Else, maps to undefined.
+ * Maps Iui's key values to SkKbd symbols (if exists) and string texts.
  */
 const IuiKbdKeysToSkKbdSymbols: Record<
 	(typeof IuiKbdKeys)[keyof typeof IuiKbdKeys],
-	React.ComponentProps<typeof SkKbd>["symbol"]
+	{
+		symbol: React.ComponentProps<typeof SkKbd>["symbol"];
+		text: string | undefined;
+	}
 > = {
-	"\u2318 Cmd": "Command",
-	"\u21e7 Shift": "Shift",
-	"\u232b Backspace": "Backspace",
-	"\u21b5 Enter": "Enter",
-	"\u23cf Eject": "Eject",
-	Ctrl: "Control",
-	"\u229e Win": undefined, // Windows
-	"\uf8ff": undefined, // Apple
-	"\u2325 Option": "Option",
-	"\u2190": "Left",
-	"\u2191": "Up",
-	"\u2192": "Right",
-	"\u2193": "Down",
+	"\u2318 Cmd": { symbol: "Command", text: "Cmd" },
+	"\u21e7 Shift": { symbol: "Shift", text: "Shift" },
+	"\u232b Backspace": { symbol: "Backspace", text: "Backspace" },
+	"\u21b5 Enter": { symbol: "Enter", text: "Enter" },
+	"\u23cf Eject": { symbol: "Eject", text: "Eject" },
+	Ctrl: { symbol: "Control", text: undefined },
+	"\u229e Win": { symbol: undefined, text: "\u229e Win" },
+	"\uf8ff": { symbol: undefined, text: "\uf8ff" }, // Apple
+	"\u2325 Option": { symbol: "Option", text: "Option" },
+	"\u2190": { symbol: "Left", text: undefined },
+	"\u2191": { symbol: "Up", text: undefined },
+	"\u2192": { symbol: "Right", text: undefined },
+	"\u2193": { symbol: "Down", text: undefined },
 };

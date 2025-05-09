8dbea6a23ab81d6a32fe2ba42197b6cba175faa0
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-05-09 09:24:30 -0400

    nit

--- apps/test-app/app/compat/toggle-switch.tsx
+++ apps/test-app/app/compat/toggle-switch.tsx
@@ -12,16 +12,17 @@ export default definePage(function Page() {
 	return (
 		<div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
 			<ToggleSwitch />
+			<ToggleSwitch checked />
 			<ToggleSwitch defaultChecked />
 			<ToggleSwitch disabled />
 			<ToggleSwitch defaultChecked disabled />
 
 			<br />
 
-			<ToggleSwitch label="HELLO" />
-			<ToggleSwitch label="HELLO" labelPosition="left" />
-			<ToggleSwitch label={<div>HELLO</div>} />
-			<ToggleSwitch label={<div>HELLO</div>} labelPosition="left" />
+			<ToggleSwitch label="Option" size="small" />
+			<ToggleSwitch label="Option" labelPosition="left" />
+			<ToggleSwitch label={<div>Option</div>} />
+			<ToggleSwitch label={<div>Option</div>} labelPosition="left" />
 		</div>
 	);
 });

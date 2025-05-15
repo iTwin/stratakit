d5c349dd954a4b81e0c8665663c0b9ca27eca5ab
Mayank <9084735+mayank99@users.noreply.github.com>
2025-05-15 11:43:10 -0400

    create `@stratakit/structures` package (#638)

--- packages/structures/types.d.ts
+++ packages/structures/types.d.ts
@@ -2,4 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-@import "../../bricks/src/styles.css";
+declare module "*.css?inline" {
+	const content: string;
+	export default content;
+}

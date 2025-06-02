6f1865f46814548d39abf1ba3e25d5fb44cbfd35
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-06-02 16:27:35 -0400

    Add `ProgressRadial` compatibility component (#700)

--- packages/compat/src/index.ts
+++ packages/compat/src/index.ts
@@ -11,6 +11,7 @@ export { Kbd, KbdKeys } from "./Kbd.js";
 export { Label } from "./Label.js";
 export { MenuItem } from "./MenuItem.js";
 export { ProgressLinear } from "./ProgressLinear.js";
+export { ProgressRadial } from "./ProgressRadial.js";
 export { Tabs } from "./Tabs.js";
 export { Tag } from "./Tag.js";
 export { Text } from "./Text.js";

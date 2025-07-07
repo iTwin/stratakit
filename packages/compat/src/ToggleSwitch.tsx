18208b32c03906416c9b013612bf20c075f7086a
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-07-07 09:34:55 -0400

    Leftover

--- packages/compat/src/ToggleSwitch.tsx
+++ packages/compat/src/ToggleSwitch.tsx
@@ -3,12 +3,14 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { Field, Switch as SkSwitch } from "@stratakit/bricks";
 import * as React from "react";
-import { useCompatProps } from "./~utils.tsx";
+import { Field, Switch as SkSwitch } from "@stratakit/bricks";
+import {
+	type PolymorphicForwardRefComponent,
+	useCompatProps,
+} from "./~utils.js";
 
 import type { ToggleSwitch as IuiSwitch } from "@itwin/itwinui-react";
-import type { PolymorphicForwardRefComponent } from "./~utils.tsx";
 
 type IuiSwitchProps = React.ComponentProps<typeof IuiSwitch>;
 
@@ -29,16 +31,20 @@ interface ToggleSwitchProps
  * In `@itwinui-react`, `ToggleSwitch`'s `className` and `style` are passed to the `<input>`. But in `@stratakit/react`,
  * they are passed to the wrapper instead.
  *
- * Thus, for a smooth transition, the `future.consistentPropsSpread` flag should be enabled in an ancestral
- * `ThemeProvider` *before* moving to `@stratakit/react`. Once in `@stratakit/react`, the flag can be removed.
+ * Thus, for a smooth transition, the `ToggleSwitch.consistentPropsSpread` future flag can be enabled in an ancestral
+ * `ThemeProvider` *before* moving to `@stratakit/react`.
  */
 export const ToggleSwitch = React.forwardRef((props, forwardedRef) => {
 	const {
 		label,
 		labelPosition = "right",
 		labelProps,
-		size, // NOT IMPLEMENTED
-		icon, // NOT IMPLEMENTED
+
+		// biome-ignore-start lint/correctness/noUnusedVariables: NOT IMPLEMENTED
+		size,
+		icon,
+		// biome-ignore-end lint/correctness/noUnusedVariables: NOT IMPLEMENTED
+
 		...rest
 	} = useCompatProps(props);
 

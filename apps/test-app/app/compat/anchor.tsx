a917ac8ab6f777591c036765ed2536dc21394931
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-07-08 17:27:53 -0400

    Add `Anchor` composition API (#757)
    Co-authored-by: Jon Graft <849817+FlyersPh9@users.noreply.github.com>

--- apps/test-app/app/compat/anchor.tsx
+++ apps/test-app/app/compat/anchor.tsx
@@ -10,7 +10,7 @@ export const handle = { title: "Anchor" };
 
 export default definePage(function Page() {
 	return (
-		<div style={{ display: "grid", gap: 4 }}>
+		<div style={{ display: "grid", gap: 4, justifyItems: "flex-start" }}>
 			<Anchor href="https://example.com">Example</Anchor>
 			<Anchor as="button">Example (as button)</Anchor>
 		</div>

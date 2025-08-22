94dd94b47849dc780c7d0e945a9c8f13c0ac6eaf
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-08-22 11:26:07 +0300

    Group tests under @a11y group

--- apps/test-app/app/tests/dialog/index.spec.ts
+++ apps/test-app/app/tests/dialog/index.spec.ts
@@ -77,41 +77,6 @@ test("action button", async ({ page }) => {
 	await expect(dialog).toBeHidden();
 });
 
-test("focus management", async ({ page }) => {
-	await page.goto("/tests/dialog");
-
-	const button = page.getByRole("button", { name: "Open" });
-	const dialog = page.getByRole("dialog");
-	const close = page.getByRole("button", { name: "Dismiss", exact: true });
-
-	// Focus the dialog on show
-	await button.click();
-	await expect(dialog).toBeFocused();
-
-	await page.keyboard.press("Tab");
-	await expect(close).toBeFocused();
-
-	// Restore focus to the disclosure on hide
-	await close.click();
-	await expect(button).toBeFocused();
-});
-
-test("inert outside dialog", async ({ page }) => {
-	await page.goto("/tests/dialog");
-
-	const button = page.getByRole("button", { name: "Open" });
-	const dialog = page.getByRole("dialog");
-
-	await button.click();
-	await expect(dialog).toBeVisible();
-
-	await expect(async () => {
-		await button.click({
-			timeout: 300,
-		});
-	}).rejects.toThrow();
-});
-
 test.describe("@visual", () => {
 	test("default", async ({ page }) => {
 		await page.goto("/tests/dialog?visual");
@@ -159,5 +124,40 @@ test.describe("@a11y", () => {
 		const axe = new AxeBuilder({ page });
 		const accessibilityScan = await axe.analyze();
 		expect(accessibilityScan.violations).toEqual([]);
+	});
+
+	test("focus management", async ({ page }) => {
+		await page.goto("/tests/dialog");
+
+		const button = page.getByRole("button", { name: "Open" });
+		const dialog = page.getByRole("dialog");
+		const close = page.getByRole("button", { name: "Dismiss", exact: true });
+
+		// Focus the dialog on show
+		await button.click();
+		await expect(dialog).toBeFocused();
+
+		await page.keyboard.press("Tab");
+		await expect(close).toBeFocused();
+
+		// Restore focus to the disclosure on hide
+		await close.click();
+		await expect(button).toBeFocused();
+	});
+
+	test("inert outside dialog", async ({ page }) => {
+		await page.goto("/tests/dialog");
+
+		const button = page.getByRole("button", { name: "Open" });
+		const dialog = page.getByRole("dialog");
+
+		await button.click();
+		await expect(dialog).toBeVisible();
+
+		await expect(async () => {
+			await button.click({
+				timeout: 300,
+			});
+		}).rejects.toThrow();
 	});
 });

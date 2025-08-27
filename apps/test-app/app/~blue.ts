5de98c19681e7a7050756f2211f7a2d746304e9c
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-08-27 19:04:35 +0300

    Simplify style loading

--- apps/test-app/app/~blue.ts
+++ apps/test-app/app/~blue.ts
@@ -6,92 +6,22 @@ import css from "./~blue.css.js";
 
 // ----------------------------------------------------------------------------
 
-function isDocument(node?: Node): node is Document {
-	return node?.nodeType === Node.DOCUMENT_NODE;
-}
+let loaded = false;
 
-function getOwnerDocument(node?: Node | null) {
-	if (!node) return null;
-	return (isDocument(node) ? node : node.ownerDocument) || null;
-}
+function loadBlueStyles() {
+	if (loaded) return () => {};
 
-const isBrowser = typeof document !== "undefined";
+	const styleSheet = new window.CSSStyleSheet();
+	styleSheet.replaceSync(css);
+	document.adoptedStyleSheets.push(styleSheet);
+	loaded = true;
 
-function getWindow(node: Node) {
-	const ownerDocument = getOwnerDocument(node);
-	return ownerDocument?.defaultView || null;
-}
-
-const supportsAdoptedStylesheets =
-	isBrowser && "adoptedStyleSheets" in Document.prototype;
-
-// ----------------------------------------------------------------------------
-
-const styleSheets = new Map<string, WeakMap<Window, CSSStyleSheet>>(
-	Object.entries({ default: new WeakMap() }),
-);
-
-function loadStyles(
-	rootNode: Document | ShadowRoot,
-	{ css, key = "default" }: { css: string; key?: string },
-) {
-	let cleanup = () => {};
-
-	const loaded = (() => {
-		if (!isBrowser) return false;
-
-		const ownerDocument = getOwnerDocument(rootNode);
-		const _window = getWindow(rootNode);
-
-		if (!ownerDocument || !_window) return false;
-
-		// Inject <style> elements if `adoptedStyleSheets` is not supported.
-		if (
-			!supportsAdoptedStylesheets &&
-			!rootNode.querySelector(`style[data-_app="${key}"]`)
-		) {
-			const styleElement = ownerDocument.createElement("style");
-			styleElement.dataset._app = key;
-			styleElement.textContent = css;
-			((rootNode as Document).head || rootNode).appendChild(styleElement);
-			cleanup = () => styleElement.remove();
-			return true;
-		}
-
-		const styleSheet =
-			styleSheets.get(key)?.get(_window) || new _window.CSSStyleSheet();
-		if (!styleSheets.get(key)?.has(_window)) {
-			styleSheets.get(key)?.set(_window, styleSheet);
-		}
-		styleSheet.replaceSync(css);
-
-		if (!rootNode.adoptedStyleSheets.includes(styleSheet)) {
-			rootNode.adoptedStyleSheets.push(styleSheet);
-
-			cleanup = () => {
-				rootNode.adoptedStyleSheets = rootNode.adoptedStyleSheets.filter(
-					(sheet) => sheet !== styleSheet,
-				);
-			};
-		}
-
-		return true;
-	})();
-
-	return { loaded, cleanup };
-}
-
-// ----------------------------------------------------------------------------
-
-function loadBlueStyles(rootNode?: Document | ShadowRoot) {
-	rootNode ||= document;
-
-	const { cleanup } = loadStyles(rootNode, {
-		css,
-		key: "sk-blue",
-	});
-
-	return cleanup;
+	return () => {
+		document.adoptedStyleSheets = document.adoptedStyleSheets.filter(
+			(sheet) => sheet !== styleSheet,
+		);
+		loaded = false;
+	};
 }
 
 // ----------------------------------------------------------------------------

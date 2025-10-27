8f8f6d34a6ee97a239cd94c075ec33abf580cbc3
Heydon Pickering <heydon.pickering@bentley.com>
2025-10-27 09:03:49 +0000

    IconButton guide (#28)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Gerardas B. <10091419+GerardasB@users.noreply.github.com>
    Co-authored-by: Mayank <9084735+mayank99@users.noreply.github.com>

---
+++ src/examples/IconButton.toolbar.tsx
@@ -0,0 +1,30 @@
+import { IconButton } from "@stratakit/bricks";
+import boldIcon from "@stratakit/icons/font-bold.svg";
+import italicIcon from "@stratakit/icons/font-italic.svg";
+import { unstable_Toolbar as Toolbar } from "@stratakit/structures";
+
+export default () => {
+	return (
+		<Toolbar.Group variant="solid">
+			<Toolbar.Item
+				render={
+					<IconButton
+						label="Bold"
+						icon={`${boldIcon}#icon-large`}
+						variant="ghost"
+						active
+					/>
+				}
+			/>
+			<Toolbar.Item
+				render={
+					<IconButton
+						label="Italic"
+						icon={`${italicIcon}#icon-large`}
+						variant="ghost"
+					/>
+				}
+			/>
+		</Toolbar.Group>
+	);
+};

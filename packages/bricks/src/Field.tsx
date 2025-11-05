2a31a5664563b5288989333659b4ba2e680a33aa
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2025-11-05 15:33:35 -0500

    @mayank99 pseudo icon

--- packages/bricks/src/Field.tsx
+++ packages/bricks/src/Field.tsx
@@ -10,7 +10,6 @@ import {
 } from "@ariakit/react/collection";
 import { Role } from "@ariakit/react/role";
 import { useStoreState } from "@ariakit/react/store";
-import { Icon } from "@stratakit/foundations";
 import { forwardRef } from "@stratakit/foundations/secret-internals";
 import cx from "classnames";
 import Description from "./Description.js";
@@ -250,23 +249,9 @@ DEV: FieldControl.displayName = "Field.Control";
 
 // ----------------------------------------------------------------------------
 
-interface FieldErrorMessageProps extends BaseProps {
-	/**
-	 * Icon to display at the start of the error message.
-	 *
-	 * Can be a URL of an SVG from the `@stratakit/icons` package,
-	 * or a custom JSX icon.
-	 *
-	 * @default Custom error icon
-	 */
-	startIcon?: string | React.JSX.Element;
-}
-
 /**
  * An associated error message for a field. When used within `<Field.Root>`, the
  * associated form control will be rendered with `aria-invalid="true"`.
- *
- * Automatically displays an error icon at the start of the message.
  *
  * Example:
  * ```tsx
@@ -276,18 +261,11 @@ interface FieldErrorMessageProps extends BaseProps {
  *   <Field.ErrorMessage>Something is wrong!</Field.ErrorMessage>
  * </Field.Root>
  * ```
- *
- * You can customize the icon:
- * ```tsx
- * <Field.ErrorMessage startIcon={<Icon href={customIcon} />}>
- *   Custom error message
- * </Field.ErrorMessage>
- * ```
  */
-const FieldErrorMessage = forwardRef<"div", FieldErrorMessageProps>(
+const FieldErrorMessage = forwardRef<"div", BaseProps>(
 	(props, forwardedRef) => {
 		const generatedId = React.useId();
-		const { id = generatedId, startIcon, children, ...rest } = props;
+		const { id = generatedId, ...rest } = props;
 
 		const getData = React.useCallback(
 			(data: CollectionStoreItem) => ({
@@ -297,57 +275,11 @@ const FieldErrorMessage = forwardRef<"div", FieldErrorMessageProps>(
 			[],
 		);
 
-		const defaultErrorIcon = (
-			<Icon
-				render={
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 16 16"
-					>
-						<path
-							fill="#df1b41"
-							d="M10.086 2H5.914a1 1 0 0 0-.707.293L2.293 5.207A1 1 0 0 0 2 5.914v4.237a1 1 0 0 0 .3.714l2.908 2.85a1 1 0 0 0 .7.285h4.178a1 1 0 0 0 .707-.293l2.914-2.914a1 1 0 0 0 .293-.707V5.914a1 1 0 0 0-.293-.707l-2.914-2.914A1 1 0 0 0 10.086 2Z"
-						/>
-						<path
-							fill="var(--stratakit-color-border-glow-strong-default)"
-							d="M10.086 3 13 5.914v4.172L10.086 13H5.908L3 10.151V5.914L5.914 3h4.172m0-1H5.914a1 1 0 0 0-.707.293L2.293 5.207A1 1 0 0 0 2 5.914v4.237a1 1 0 0 0 .3.714l2.908 2.85a1 1 0 0 0 .7.285h4.178a1 1 0 0 0 .707-.293l2.914-2.914a1 1 0 0 0 .293-.707V5.914a1 1 0 0 0-.293-.707l-2.914-2.914A1 1 0 0 0 10.086 2Z"
-						/>
-						<path
-							fill="#000"
-							opacity=".48"
-							d="M8 4c-.514 0-.982.24-1.282.657-.301.417-.38.936-.218 1.424l.788 2.365a.75.75 0 0 0 1.424 0L9.5 6.081a1.564 1.564 0 0 0-.218-1.424A1.565 1.565 0 0 0 8 4Zm0 5.25c-.758 0-1.375.617-1.375 1.375S7.242 12 8 12s1.375-.617 1.375-1.375S8.758 9.25 8 9.25Z"
-						/>
-						<path
-							fill="#fff"
-							d="M8 9.75a.875.875 0 1 1 0 1.75.875.875 0 0 1 0-1.75ZM8 4.5c.738 0 1.26.723 1.026 1.423l-.789 2.365a.25.25 0 0 1-.474 0l-.788-2.365A1.081 1.081 0 0 1 8 4.5Z"
-						/>
-					</svg>
-				}
-			/>
-		);
-
-		const iconElement =
-			startIcon !== undefined ? (
-				typeof startIcon === "string" ? (
-					<Icon href={startIcon} />
-				) : (
-					startIcon
-				)
-			) : (
-				defaultErrorIcon
-			);
-
 		return (
 			<CollectionItem
 				id={id}
 				getItem={getData}
-				render={
-					<Description {...rest} tone="critical">
-						{iconElement}
-						{children}
-					</Description>
-				}
+				render={<Description {...rest} tone="critical" />}
 				ref={forwardedRef}
 			/>
 		);

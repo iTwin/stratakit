1f970b4eee0ab6cfda850aaa12a8fc511a2433a7
Heydon Pickering <heydon.pickering@bentley.com>
2025-11-10 08:34:24 +0000

    Accordion doc (#49)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Mayank <9084735+mayank99@users.noreply.github.com>

---
+++ src/examples/AccordionItem.marker-right.tsx
@@ -0,0 +1,18 @@
+import { unstable_AccordionItem as AccordionItem } from "@stratakit/structures";
+
+export default () => {
+	return (
+		<AccordionItem.Root>
+			<AccordionItem.Header>
+				<AccordionItem.Button>
+					<AccordionItem.Label>What is StrataKit?</AccordionItem.Label>
+				</AccordionItem.Button>
+				<AccordionItem.Marker />
+			</AccordionItem.Header>
+			<AccordionItem.Content>
+				StrataKit is Bentley Systems' open source design system and the
+				successor to iTwinUI.
+			</AccordionItem.Content>
+		</AccordionItem.Root>
+	);
+};

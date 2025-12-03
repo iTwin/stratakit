866a9e0ef5adad1c0f479bc85671ab2249d5f977
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-12-03 18:35:10 +0200

    Simplify createTheme

--- packages/mui/src/createTheme.ts
+++ packages/mui/src/createTheme.ts
@@ -16,21 +16,16 @@ function createTheme() {
 				palette: {
 					contrastThreshold: 4.5,
 					primary: {
-						main: "var(--stratakit-color-bg-accent-base, oklch(54% .1 170.26))",
+						main: "var(--stratakit-color-bg-accent-base)",
 					},
 					text: {
-						primary:
-							"var(--stratakit-color-text-neutral-primary, oklch(29.76% .011 268.32))",
-						secondary:
-							"var(--stratakit-color-text-neutral-secondary, oklch(41.45% .013 256.75))",
+						primary: "var(--stratakit-color-text-neutral-primary)",
+						secondary: "var(--stratakit-color-text-neutral-secondary)",
 					},
-					divider:
-						"var(--stratakit-color-border-neutral-muted, oklch(91.52% .005 258.33))",
+					divider: "var(--stratakit-color-border-neutral-muted)",
 					background: {
-						default:
-							"var(--stratakit-color-bg-page-base, oklch(99.06% .002 247.84))",
-						paper:
-							"var(--stratakit-color-bg-elevation-base, oklch(100% none none))",
+						default: "var(--stratakit-color-bg-page-base)",
+						paper: "var(--stratakit-color-bg-elevation-base)",
 					},
 				},
 			},
@@ -38,23 +33,17 @@ function createTheme() {
 				palette: {
 					contrastThreshold: 4.5,
 					primary: {
-						main: "var(--stratakit-color-bg-accent-base, oklch(54% .1 170.26))",
-						contrastText:
-							"var(--stratakit-color-icon-neutral-emphasis, oklch(100% none none))",
+						main: "var(--stratakit-color-bg-accent-base)",
+						contrastText: "var(--stratakit-color-icon-neutral-emphasis)",
 					},
 					text: {
-						primary:
-							"var(--stratakit-color-text-neutral-primary, oklch(99.06% .002 247.84))",
-						secondary:
-							"var(--stratakit-color-text-neutral-secondary, oklch(79.56% .008 241.69))",
+						primary: "var(--stratakit-color-text-neutral-primary)",
+						secondary: "var(--stratakit-color-text-neutral-secondary)",
 					},
-					divider:
-						"var(--stratakit-color-border-neutral-muted, oklch(32.63% .014 268.32))",
+					divider: "var(--stratakit-color-border-neutral-muted)",
 					background: {
-						default:
-							"var(--stratakit-color-bg-page-base, oklch(24.37% .006 268.32))",
-						paper:
-							"var(--stratakit-color-bg-elevation-base, oklch(26.92% .008 268.32))",
+						default: "var(--stratakit-color-bg-page-base)",
+						paper: "var(--stratakit-color-bg-elevation-base)",
 					},
 				},
 			},
@@ -67,9 +56,6 @@ function createTheme() {
 			create: () => "none", // https://mui.com/material-ui/getting-started/faq/#how-can-i-disable-transitions-globally
 		},
 		components: {
-			MuiCssBaseline: {
-				styleOverrides: `${fontsCss}${focusOutlineCss}`,
-			},
 			MuiButtonBase: {
 				defaultProps: {
 					disableRipple: true, // https://mui.com/material-ui/getting-started/faq/#how-can-i-disable-the-ripple-effect-globally
@@ -83,63 +69,6 @@ function createTheme() {
 		},
 	});
 }
-
-// ----------------------------------------------------------------------------
-
-const fontsCss = `\
-@font-face {
-  font-family: InterVariable;
-  font-style: normal;
-  font-weight 100 900;
-  font-display: swap;
-  src: url("https://rsms.me/inter/font-files/InterVariable.woff2?v=4.1") format("woff2");
-}
-@font-face {
-  font-family: InterVariable;
-  font-style: italic;
-  font-weight 100 900;
-  font-display: swap;
-  src: url("https://rsms.me/inter/font-files/InterVariable-Italic.woff2?v=4.1") format("woff2");
-}
-@font-feature-values InterVariable {
-  @character-variant {
-    cv01: 1; cv02: 2; cv03: 3; cv04: 4; cv05: 5; cv06: 6; cv07: 7; cv08: 8;
-    cv09: 9; cv10: 10; cv11: 11; cv12: 12; cv13: 13;
-    alt-1: 1;
-    alt-3: 9;
-    open-4: 2;
-    open-6: 3;
-    open-9: 4;
-    lc-l-with-tail: 5;
-    simplified-u: 6;
-    alt-double-s: 7;
-    uc-i-with-serif: 8;
-    uc-g-with-spur: 10;
-    single-story-a: 11;
-    compact-lc-f: 12;
-    compact-lc-t: 13;
-  }
-  @styleset {
-    ss01: 1; ss02: 2; ss03: 3; ss04: 4; ss05: 5; ss06: 6; ss07: 7; ss08: 8;
-    open-digits: 1;
-    disambiguation: 2;
-    disambiguation-except-zero: 4;
-    round-quotes-and-commas: 3;
-    square-punctuation: 7;
-    square-quotes: 8;
-    circled-characters: 5;
-    squared-characters: 6;
-  }
-}
-body {
-  font-variant-alternates: character-variant(alt-1, alt-3, lc-l-with-tail, uc-i-with-serif);
-  font-size: 0.75rem;
-}
-`;
-
-const focusOutlineCss = `\
-*:focus-visible { outline: 2px solid var(--mui-palette-primary-text, CanvasText) !important; outline-offset: 2px; }
-`;
 
 // ----------------------------------------------------------------------------
 

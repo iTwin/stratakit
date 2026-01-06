6718d98c064eca9068641b9fc27ab1328940867d
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-01-07 01:46:14 +0200

    Avoid aria-description

--- examples/mui/Badge.default.tsx
+++ examples/mui/Badge.default.tsx
@@ -3,17 +3,22 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import { useId } from "react";
 import { Badge, IconButton, Tooltip } from "@mui/material";
 import { Icon } from "@stratakit/mui";
 
 import emailIcon from "@stratakit/icons/email.svg";
 
 export default () => {
+	const descriptionId = useId();
 	return (
 		<Tooltip title="Inbox">
-			<IconButton aria-description="You have 4 new messages">
+			<IconButton aria-describedby={descriptionId}>
 				<Badge badgeContent={4} color="primary">
 					<Icon href={`${emailIcon}#icon-large`} size="large" />
+					<span id={descriptionId} hidden>
+						You have 4 new messages
+					</span>
 				</Badge>
 			</IconButton>
 		</Tooltip>

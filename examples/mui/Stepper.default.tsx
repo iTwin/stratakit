a72bd0fe87e60fc080ee515ccf57b78a52fd512e
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-01-07 16:08:45 +0200

    Use list semantics in Stepper

--- examples/mui/Stepper.default.tsx
+++ examples/mui/Stepper.default.tsx
@@ -7,18 +7,18 @@ import { Step, StepLabel, Stepper, Typography } from "@mui/material";
 
 export default () => {
 	return (
-		<Stepper activeStep={2}>
-			<Step>
+		<Stepper activeStep={2} role="list">
+			<Step role="listitem">
 				<StepLabel>Select campaign settings</StepLabel>
 			</Step>
-			<Step completed={false}>
+			<Step role="listitem" completed={false}>
 				<StepLabel
 					optional={<Typography variant="caption">Optional</Typography>}
 				>
 					Create an ad group
 				</StepLabel>
 			</Step>
-			<Step>
+			<Step role="listitem" aria-current="true">
 				<StepLabel>Create an ad</StepLabel>
 			</Step>
 		</Stepper>

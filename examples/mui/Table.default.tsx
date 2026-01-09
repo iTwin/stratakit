b5a029b3c638101b7b63abfb2d44515e73c35f50
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-01-09 18:08:59 +0200

    Rm TableSortLabel

--- examples/mui/Table.default.tsx
+++ examples/mui/Table.default.tsx
@@ -10,7 +10,6 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
 
 import styles from "./Table.default.module.css";
 
@@ -40,11 +39,7 @@ export default () => {
 			<Table className={styles.table} aria-label="simple table">
 				<TableHead>
 					<TableRow>
-						<TableCell sortDirection="asc">
-							<TableSortLabel active direction="asc">
-								Dessert (100g serving)
-							</TableSortLabel>
-						</TableCell>
+						<TableCell>Dessert (100g serving)</TableCell>
 						<TableCell align="right">Calories</TableCell>
 						<TableCell align="right">Fat&nbsp;(g)</TableCell>
 						<TableCell align="right">Carbs&nbsp;(g)</TableCell>

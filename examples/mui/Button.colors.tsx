88d13b207170b8663956887d0c4539d9863ff531
Mayank <9084735+mayank99@users.noreply.github.com>
2026-01-12 11:20:23 -0500

    update examples

--- examples/mui/Button.colors.tsx
+++ examples/mui/Button.colors.tsx
@@ -10,9 +10,6 @@ export default () => {
 		<>
 			<Button color="primary">Primary</Button>
 			<Button color="secondary">Secondary</Button>
-			<Button color="info">Info</Button>
-			<Button color="success">Success</Button>
-			<Button color="warning">Warning</Button>
 			<Button color="error">Error</Button>
 		</>
 	);

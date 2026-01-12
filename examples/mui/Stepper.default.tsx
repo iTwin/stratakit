24dcab6144b436631b40a6331ae199ba9d8506e0
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-01-12 10:47:46 +0200

    Update Stepper example

--- examples/mui/Stepper.default.tsx
+++ examples/mui/Stepper.default.tsx
@@ -6,17 +6,21 @@
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Stepper from "@mui/material/Stepper";
+import { visuallyHidden } from "@mui/utils";
 
 export default () => {
 	return (
-		<Stepper activeStep={2} role="list">
+		<Stepper activeStep={1} role="list">
 			<Step role="listitem">
-				<StepLabel>Select campaign settings</StepLabel>
-			</Step>
-			<Step role="listitem">
-				<StepLabel>Create an ad group</StepLabel>
+				<StepLabel>
+					Select campaign settings
+					<span style={visuallyHidden}> (completed)</span>
+				</StepLabel>
 			</Step>
 			<Step role="listitem" aria-current="true">
+				<StepLabel>Create an ad group</StepLabel>
+			</Step>
+			<Step role="listitem">
 				<StepLabel>Create an ad</StepLabel>
 			</Step>
 		</Stepper>

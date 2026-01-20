abf74eb127f58bc24596229dd667ee1c2b734142
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-01-20 11:14:08 +0200

    Refactor List.subheader

--- examples/mui/List.subheader.tsx
+++ examples/mui/List.subheader.tsx
@@ -13,18 +13,32 @@ import styles from "./List.subheader.module.css";
 export default () => {
 	return (
 		<List className={styles.list}>
-			{Array.from({ length: 4 }).map((_, sectionIndex) => (
-				<li key={sectionIndex}>
-					<ul>
-						<ListSubheader>Section {sectionIndex}</ListSubheader>
-						{Array.from({ length: 4 }).map((_, itemIndex) => (
-							<ListItem key={itemIndex}>
-								<ListItemText primary={`Item ${itemIndex}`} />
-							</ListItem>
-						))}
-					</ul>
-				</li>
-			))}
+			<SectionItem label="Section 0" />
+			<SectionItem label="Section 1" />
+			<SectionItem label="Section 2" />
+			<SectionItem label="Section 3" />
 		</List>
 	);
 };
+
+function SectionItem(props: { label: string }) {
+	return (
+		<li>
+			<ul>
+				<ListSubheader>{props.label}</ListSubheader>
+				<ListItem>
+					<ListItemText primary="Item 0" />
+				</ListItem>
+				<ListItem>
+					<ListItemText primary="Item 1" />
+				</ListItem>
+				<ListItem>
+					<ListItemText primary="Item 2" />
+				</ListItem>
+				<ListItem>
+					<ListItemText primary="Item 3" />
+				</ListItem>
+			</ul>
+		</li>
+	);
+}

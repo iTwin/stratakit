b23bd931f57e12ec5c87c2f871af141e1acde4ee
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-01-28 15:34:36 -0500

    Add checkbox & radio error styling, add samples.

---
+++ examples/mui/FormControl.error.tsx
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import Checkbox from "@mui/material/Checkbox";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormLabel from "@mui/material/FormLabel";
+
+export default () => {
+	return (
+		<FormControl component="fieldset" variant="standard" error>
+			<FormLabel component="legend">Privacy preferences</FormLabel>
+			<FormGroup>
+				<FormControlLabel control={<Checkbox />} label="Allow cookies" />
+				<FormControlLabel
+					control={<Checkbox />}
+					label="Allow personalized ads"
+				/>
+				<FormControlLabel
+					control={<Checkbox />}
+					label="Allow location access"
+				/>
+			</FormGroup>
+			<FormHelperText>
+				You must accept these settings to continue.
+			</FormHelperText>
+		</FormControl>
+	);
+};

7cbc38f8b9bab4d7685bd573520113560016e207
Gerardas B. <10091419+GerardasB@users.noreply.github.com>
2026-02-10 19:05:36 +0200

    Add-back `Button` docs (#1203)
    Co-authored-by: Heydon Pickering <heydon@heydonworks.com>

--- examples/mui/Button.colors.tsx
+++ examples/mui/Button.colors.tsx
@@ -4,13 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 export default () => {
 	return (
-		<>
+		<Stack spacing={1} direction="row">
 			<Button color="primary">Primary</Button>
 			<Button color="secondary">Secondary</Button>
 			<Button color="error">Error</Button>
-		</>
+		</Stack>
 	);
 };

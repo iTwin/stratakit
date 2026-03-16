b9eea6946f00df5a48679757fef622c65f768822
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-03-16 18:48:13 +0200

    Examples

---
+++ examples/mui/Avatar.initials.tsx
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import Avatar from "@mui/material/Avatar";
+
+export default () => {
+	return (
+		<Avatar aria-label="Greg Bentley" role="img">
+			<abbr aria-hidden="true">GB</abbr>
+		</Avatar>
+	);
+};

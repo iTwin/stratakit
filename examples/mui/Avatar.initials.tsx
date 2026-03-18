c4898afcfbeea63e04e8f20bf6ec5ac4df5b49fb
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-03-18 10:03:03 -0400

    Changed aria-label for initials example

--- examples/mui/Avatar.initials.tsx
+++ examples/mui/Avatar.initials.tsx
@@ -7,8 +7,8 @@ import Avatar from "@mui/material/Avatar";
 
 export default () => {
 	return (
-		<Avatar aria-label="Greg Bentley" role="img">
-			<abbr aria-hidden="true">GB</abbr>
+		<Avatar aria-label="Willow Winters" role="img">
+			<abbr aria-hidden="true">WW</abbr>
 		</Avatar>
 	);
 };

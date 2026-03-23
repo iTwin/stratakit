10a7893e143794ae9ec5e35e65acd99e7f5f3f6d
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-03-23 11:45:50 +0200

    Fix default example

--- examples/mui/Skeleton.default.tsx
+++ examples/mui/Skeleton.default.tsx
@@ -4,13 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 
 import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 import visuallyHidden from "@mui/utils/visuallyHidden";
 
 export default () => {
 	return (
-		<>
+		<Stack flexGrow={1} alignSelf="stretch">
 			<Skeleton />
 			<div style={visuallyHidden}>Loading...</div>
-		</>
+		</Stack>
 	);
 };

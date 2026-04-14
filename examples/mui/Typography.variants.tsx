864e241f0ff59e83ef2d14cdd63f3348a55f26bd
Mayank <9084735+mayank99@users.noreply.github.com>
2026-04-14 12:49:33 -0400

    fix `Typography` docs (#1419)

--- examples/mui/Typography.variants.tsx
+++ examples/mui/Typography.variants.tsx
@@ -13,16 +13,32 @@ export default () => {
 			<Typography variant="body2">Body2</Typography>
 			<Typography variant="button">Button</Typography>
 			<Typography variant="caption">Caption</Typography>
-			<Typography variant="h1">H1</Typography>
-			<Typography variant="h2">H2</Typography>
-			<Typography variant="h3">H3</Typography>
-			<Typography variant="h4">H4</Typography>
-			<Typography variant="h5">H5</Typography>
-			<Typography variant="h6">H6</Typography>
+			<Typography variant="h1" render={<div />}>
+				H1
+			</Typography>
+			<Typography variant="h2" render={<div />}>
+				H2
+			</Typography>
+			<Typography variant="h3" render={<div />}>
+				H3
+			</Typography>
+			<Typography variant="h4" render={<div />}>
+				H4
+			</Typography>
+			<Typography variant="h5" render={<div />}>
+				H5
+			</Typography>
+			<Typography variant="h6" render={<div />}>
+				H6
+			</Typography>
 			<Typography variant="inherit">inherit</Typography>
 			<Typography variant="overline">overline</Typography>
-			<Typography variant="subtitle1">subtitle1</Typography>
-			<Typography variant="subtitle2">subtitle2</Typography>
+			<Typography variant="subtitle1" render={<div />}>
+				subtitle1
+			</Typography>
+			<Typography variant="subtitle2" render={<div />}>
+				subtitle2
+			</Typography>
 		</Stack>
 	);
 };

2c69429e304ea197b56a51868624ebd324e33370
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-04-15 12:37:33 -0400

    Objects collapsed into single line

--- examples/mui/Slider.marks.tsx
+++ examples/mui/Slider.marks.tsx
@@ -9,30 +9,12 @@ import FormLabel from "@mui/material/FormLabel";
 import Slider from "@mui/material/Slider";
 
 const marks = [
-	{
-		value: 0,
-		label: "0°C",
-	},
-	{
-		value: 20,
-		label: "20°C",
-	},
-	{
-		value: 40,
-		label: "40°C",
-	},
-	{
-		value: 60,
-		label: "60°C",
-	},
-	{
-		value: 80,
-		label: "80°C",
-	},
-	{
-		value: 100,
-		label: "100°C",
-	},
+	{ value: 0, label: "0°C" },
+	{ value: 20, label: "20°C" },
+	{ value: 40, label: "40°C" },
+	{ value: 60, label: "60°C" },
+	{ value: 80, label: "80°C" },
+	{ value: 100, label: "100°C" },
 ];
 
 export default () => {

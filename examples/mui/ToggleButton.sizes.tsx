78d7d0d4c6405a72a3f92bc564f905c679b5ce7e
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-04-21 11:09:54 +0300

    Simplify sizes example

--- examples/mui/ToggleButton.sizes.tsx
+++ examples/mui/ToggleButton.sizes.tsx
@@ -3,7 +3,6 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import * as React from "react";
 import Stack from "@mui/material/Stack";
 import ToggleButton from "@mui/material/ToggleButton";
 import { Icon } from "@stratakit/mui";
@@ -11,51 +10,17 @@ import { Icon } from "@stratakit/mui";
 import svgPlaceholder from "@stratakit/icons/placeholder.svg";
 
 export default () => {
-	const [isSelected, onChange] = useSelected();
 	return (
 		<Stack spacing={1} direction="row" sx={{ alignItems: "center" }}>
-			<ToggleButton
-				value="small"
-				size="small"
-				label="Small"
-				selected={isSelected("small")}
-				onChange={onChange}
-			>
+			<ToggleButton value="small" size="small" label="Small">
 				<Icon href={svgPlaceholder} />
 			</ToggleButton>
-			<ToggleButton
-				value="medium"
-				label="Medium"
-				selected={isSelected("medium")}
-				onChange={onChange}
-			>
+			<ToggleButton value="medium" label="Medium">
 				<Icon href={svgPlaceholder} />
 			</ToggleButton>
-			<ToggleButton
-				value="large"
-				size="large"
-				label="Large"
-				selected={isSelected("large")}
-				onChange={onChange}
-			>
+			<ToggleButton value="large" size="large" label="Large">
 				<Icon href={svgPlaceholder} />
 			</ToggleButton>
 		</Stack>
 	);
 };
-
-type ToggleButtonProps = React.ComponentProps<typeof ToggleButton>;
-
-function useSelected() {
-	const [selected, setSelected] = React.useState<string[]>([]);
-	const onChange = ((_, value) => {
-		setSelected((prev) => {
-			if (prev.includes(value)) {
-				return prev.filter((v) => v !== value);
-			}
-			return [...prev, value];
-		});
-	}) satisfies ToggleButtonProps["onChange"];
-	const isSelected = (value: string) => selected.includes(value);
-	return [isSelected, onChange] as const;
-}

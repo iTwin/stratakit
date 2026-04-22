28573350aca6b6c698e648e400d4818c4b8cac7a
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-04-22 15:56:29 -0400

    Add `Slider`

---
+++ examples/mui/Slider.sizes.tsx
@@ -0,0 +1,51 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as React from "react";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Slider from "@mui/material/Slider";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { visuallyHidden } from "@mui/utils";
+
+export default () => {
+	const id = React.useId();
+	return (
+		<Stack spacing={1} sx={{ alignSelf: "stretch" }}>
+			<FormControl fullWidth>
+				<FormLabel htmlFor={id}>
+					Small size
+					<span style={visuallyHidden}> from 0 to 100 inches</span>
+				</FormLabel>
+				<Stack spacing={1} direction="row" sx={{ alignItems: "center", mb: 1 }}>
+					<Typography aria-hidden="true">0″</Typography>
+					<Slider
+						defaultValue={50}
+						slotProps={{ input: { id } }}
+						size="small"
+					/>
+					<Typography aria-hidden="true">100″</Typography>
+				</Stack>
+			</FormControl>
+
+			<FormControl fullWidth>
+				<FormLabel htmlFor={id}>
+					Medium size
+					<span style={visuallyHidden}> from 0 to 100 feet</span>
+				</FormLabel>
+				<Stack spacing={1} direction="row" sx={{ alignItems: "center", mb: 1 }}>
+					<Typography aria-hidden="true" sx={{ textWrap: "nowrap" }}>
+						0 ft
+					</Typography>
+					<Slider defaultValue={50} slotProps={{ input: { id } }} />
+					<Typography aria-hidden="true" sx={{ textWrap: "nowrap" }}>
+						100 ft
+					</Typography>
+				</Stack>
+			</FormControl>
+		</Stack>
+	);
+};

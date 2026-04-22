6e3e377c457819877b7d45f6f683542014920529
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-04-22 12:58:08 -0400

    Simplify example

--- examples/mui/Table.select.tsx
+++ examples/mui/Table.select.tsx
@@ -20,9 +20,6 @@ interface RowData {
 	id: number;
 	name: string;
 	calories: number;
-	fat: number;
-	carbs: number;
-	protein: number;
 }
 
 export default () => {
@@ -31,41 +28,26 @@ export default () => {
 			id: 1,
 			name: "Cupcake",
 			calories: 305,
-			fat: 3.7,
-			carbs: 67,
-			protein: 4.3,
 		},
 		{
 			id: 2,
 			name: "Eclair",
 			calories: 262,
-			fat: 16.0,
-			carbs: 24,
-			protein: 6.0,
 		},
 		{
 			id: 3,
 			name: "Frozen yoghurt",
 			calories: 159,
-			fat: 6.0,
-			carbs: 24,
-			protein: 4.0,
 		},
 		{
 			id: 4,
 			name: "Gingerbread",
 			calories: 356,
-			fat: 16.0,
-			carbs: 49,
-			protein: 3.9,
 		},
 		{
 			id: 5,
 			name: "Ice cream sandwich",
 			calories: 237,
-			fat: 9.0,
-			carbs: 37,
-			protein: 4.3,
 		},
 	];
 
@@ -108,9 +90,6 @@ export default () => {
 						<TableCell padding="checkbox" />
 						<TableCell>Dessert (100g serving)</TableCell>
 						<TableCell align="right">Calories</TableCell>
-						<TableCell align="right">Fat&nbsp;(g)</TableCell>
-						<TableCell align="right">Carbs&nbsp;(g)</TableCell>
-						<TableCell align="right">Protein&nbsp;(g)</TableCell>
 					</TableRow>
 				</TableHead>
 				<TableBody>
@@ -139,9 +118,6 @@ export default () => {
 								</TableCell>
 								<TableCell>{row.name}</TableCell>
 								<TableCell align="right">{row.calories}</TableCell>
-								<TableCell align="right">{row.fat}</TableCell>
-								<TableCell align="right">{row.carbs}</TableCell>
-								<TableCell align="right">{row.protein}</TableCell>
 							</TableRow>
 						);
 					})}

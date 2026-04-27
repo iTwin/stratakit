f5f33af0602fbd5d769922f1303729dd0677ac5c
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-04-27 11:29:24 -0400

    Update `ToggleButton` to be more complex

--- examples/mui/ToggleButton._disabled.tsx
+++ examples/mui/ToggleButton._disabled.tsx
@@ -3,15 +3,37 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import * as React from "react";
 import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import { Icon } from "@stratakit/mui";
 
-import svgPlaceholder from "@stratakit/icons/placeholder.svg";
+import svgTextAlignCenter from "@stratakit/icons/text-align-center.svg";
+import svgTextAlignJustify from "@stratakit/icons/text-align-justify.svg";
+import svgTextAlignLeft from "@stratakit/icons/text-align-left.svg";
+import svgTextAlignRight from "@stratakit/icons/text-align-right.svg";
 
 export default () => {
+	const [alignment, setAlignment] = React.useState(["center"]);
 	return (
-		<ToggleButton value="disabled" label="Disabled" disabled>
-			<Icon href={svgPlaceholder} />
-		</ToggleButton>
+		<ToggleButtonGroup
+			disabled
+			value={alignment}
+			onChange={(_, newAlignment) => setAlignment(newAlignment)}
+			aria-label="text alignment"
+		>
+			<ToggleButton value="left" label="Left aligned">
+				<Icon href={svgTextAlignLeft} />
+			</ToggleButton>
+			<ToggleButton value="center" label="Centered">
+				<Icon href={svgTextAlignCenter} />
+			</ToggleButton>
+			<ToggleButton value="right" label="Right aligned">
+				<Icon href={svgTextAlignRight} />
+			</ToggleButton>
+			<ToggleButton value="justify" label="Justified">
+				<Icon href={svgTextAlignJustify} />
+			</ToggleButton>
+		</ToggleButtonGroup>
 	);
 };

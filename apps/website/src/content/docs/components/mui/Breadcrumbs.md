b87c90c21af3f5b148603cb202b02231f88f7f21
Mayank <9084735+mayank99@users.noreply.github.com>
2026-05-21 19:56:11 -0400

    add `aria-current` to Breadcrumbs

--- apps/website/src/content/docs/components/mui/Breadcrumbs.md
+++ apps/website/src/content/docs/components/mui/Breadcrumbs.md
@@ -7,3 +7,7 @@ links:
 ---
 
 ::example{src="mui/Breadcrumbs.default"}
+
+## StrataKit MUI modifications
+
+- Updated the examples to use [`aria-current`](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Attributes/aria-current) on the current breadcrumb for improved accessibility.
--- examples/mui/Breadcrumbs.default.tsx
+++ examples/mui/Breadcrumbs.default.tsx
@@ -12,7 +12,9 @@ export default () => {
 		<Breadcrumbs aria-label="breadcrumb">
 			<Link href="/">Home</Link>
 			<Link href="#packages">Packages</Link>
-			<Typography color="text.primary">@stratakit/mui</Typography>
+			<Typography render={<a />} aria-current="true" color="text.primary">
+				@stratakit/mui
+			</Typography>
 		</Breadcrumbs>
 	);
 };

b99e01062347253c5092c225b058b40c91c01208
Mayank <9084735+mayank99@users.noreply.github.com>
2026-05-25 18:28:28 -0400

    remove underline from static `Link`

--- examples/mui/Breadcrumbs.default.tsx
+++ examples/mui/Breadcrumbs.default.tsx
@@ -5,16 +5,15 @@
 
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
-import Typography from "@mui/material/Typography";
 
 export default () => {
 	return (
 		<Breadcrumbs aria-label="breadcrumb">
 			<Link href="/">Home</Link>
 			<Link href="#packages">Packages</Link>
-			<Typography render={<a />} aria-current="true" color="text.primary">
+			<Link aria-current="true" color="textSecondary">
 				@stratakit/mui
-			</Typography>
+			</Link>
 		</Breadcrumbs>
 	);
 };

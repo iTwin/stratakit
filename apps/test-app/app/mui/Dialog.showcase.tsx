53916f8e1d6ffe69809216a09cb24237c51df6f4
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-06-30 10:42:05 -0400

    Dialog

--- apps/test-app/app/mui/Dialog.showcase.tsx
+++ apps/test-app/app/mui/Dialog.showcase.tsx
@@ -3,7 +3,13 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import DialogDefault from "examples/mui/Dialog.default.tsx";
+import DialogFullScreen from "examples/mui/Dialog.fullScreen.tsx";
 
 export default function DialogExamples() {
-	return <DialogDefault />;
+	return (
+		<>
+			<DialogDefault />
+			<DialogFullScreen />
+		</>
+	);
 }

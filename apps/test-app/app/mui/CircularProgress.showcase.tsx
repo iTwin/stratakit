f05f5b2563b8918e162c6304d2f2972771894eb7
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-07-01 13:30:51 -0400

    CircularProgress

--- apps/test-app/app/mui/CircularProgress.showcase.tsx
+++ apps/test-app/app/mui/CircularProgress.showcase.tsx
@@ -5,12 +5,14 @@
 import Stack from "@mui/material/Stack";
 import CircularProgressColors_ from "examples/mui/CircularProgress._colors.tsx";
 import CircularProgressDefault from "examples/mui/CircularProgress.default.tsx";
+import CircularProgressDeterminate from "examples/mui/CircularProgress.determinate.tsx";
 import { isProduction } from "~/~utils.tsx";
 
 export default function CircularProgressExamples() {
 	return (
 		<>
 			<CircularProgressDefault />
+			<CircularProgressDeterminate />
 			{!isProduction && (
 				<Stack spacing={1} direction="row">
 					<CircularProgressColors_ />

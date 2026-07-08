8d9b4d0add0339c7f1340c8cdb97a0df48e02c06
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-07-08 11:13:09 -0400

    Deprecate props for Switch

--- packages/mui/src/types.ts
+++ packages/mui/src/types.ts
@@ -18,6 +18,7 @@ import type {
 	OverridableTypeMap,
 } from "@mui/material/OverridableComponent";
 import type { SvgIconProps } from "@mui/material/SvgIcon";
+import type { SwitchProps } from "@mui/material/Switch";
 import type { TabProps } from "@mui/material/Tab";
 import type { TableCellProps as MuiTableCellProps } from "@mui/material/TableCell";
 import type { TabsProps } from "@mui/material/Tabs";

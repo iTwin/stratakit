1d0ba0f07695f288dfaa6c15265f2c341bb94c44
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-07-08 11:13:09 -0400

    Deprecate props for Switch

--- packages/mui/src/types.ts
+++ packages/mui/src/types.ts
@@ -20,6 +20,7 @@ import type {
 } from "@mui/material/OverridableComponent";
 import type { RadioProps } from "@mui/material/Radio";
 import type { SvgIconProps } from "@mui/material/SvgIcon";
+import type { SwitchProps } from "@mui/material/Switch";
 import type { TabProps } from "@mui/material/Tab";
 import type { TableCellProps as MuiTableCellProps } from "@mui/material/TableCell";
 import type { TabsProps } from "@mui/material/Tabs";
@@ -590,6 +591,17 @@ declare module "@mui/material/Switch" {
 		success: false;
 		warning: false;
 		error: false;
+	}
+
+	interface SwitchOwnProps {
+		/** @deprecated DO NOT USE. */
+		checkedIcon?: SwitchProps["checkedIcon"];
+
+		/** @deprecated DO NOT USE. */
+		disableRipple?: SwitchProps["disableRipple"];
+
+		/** @deprecated DO NOT USE. */
+		icon?: SwitchProps["icon"];
 	}
 }
 

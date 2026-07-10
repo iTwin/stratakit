fe99329b982321cf855fcc716e91e3d24bdc8e61
Mark Dane <mark@cesium.com>
2026-07-10 15:27:26 +0000

    better divider exmaples

--- examples/mui/Divider.margin.tsx
+++ examples/mui/Divider.margin.tsx
@@ -3,30 +3,17 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import React from "react";
 import Divider from "@mui/material/Divider";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import Stack from "@mui/material/Stack";
-import Switch from "@mui/material/Switch";
 import Typography from "@mui/material/Typography";
 
 export default () => {
-	const [margin, setMargin] = React.useState(true);
-
 	return (
 		<Stack>
 			<Typography>Item</Typography>
-			<Divider flexItem margin={margin} />
-			<FormControlLabel
-				control={
-					<Switch
-						checked={margin}
-						onChange={(event) => setMargin(event.target.checked)}
-					/>
-				}
-				label="Margin"
-			/>
-			<Divider flexItem margin={margin} />
+			<Divider flexItem margin />
+			<Typography>Item</Typography>
+			<Divider flexItem margin />
 			<Typography>Item</Typography>
 		</Stack>
 	);

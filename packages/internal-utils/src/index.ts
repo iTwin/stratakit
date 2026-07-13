c3e5fe6735b2e23534ec07d71b0ab60f7c054f08
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-07-13 12:06:20 +0300

    Rename and split-up utils.ts

--- packages/internal-utils/src/index.ts
+++ packages/internal-utils/src/index.ts
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+export * from "./common.js";
+export * from "./dom.js";
 export * from "./hooks.js";
-export * from "./icons.js";
-export * from "./symbol.js";
-export * from "./utils.js";
+export * from "./react.js";

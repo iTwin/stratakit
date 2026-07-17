54e224635811a7003216f6b14ebf22c841c4983b
Jason Wichert <82613512+jasonjwichert@users.noreply.github.com>
2026-07-17 09:46:16 -0400

    update comment

--- packages/mui/src/~components/MuiAvatarGroup.tsx
+++ packages/mui/src/~components/MuiAvatarGroup.tsx
@@ -18,7 +18,7 @@ const MuiAvatarGroup = forwardRef<"div", MuiAvatarGroupProps>(
 		const { children: childrenProp, ...rest } = props;
 
 		const children = React.Children.map(childrenProp, (child) => {
-			// when no surplus chip is present, the first child is null, we must skip it
+			// When no surplus chip is present, the first child is null. We must skip it,
 			// or it will render an empty list item, causing incorrect mask application.
 			if (child == null) return null;
 

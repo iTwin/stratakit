d6fcb44d21d83f49e3d49548edf5d42b7c218cda
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-07-27 15:48:37 +0300

    Update examples

--- examples/structures/Tree.select-many.tsx
+++ examples/structures/Tree.select-many.tsx
@@ -3,169 +3,131 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import Stack from "@mui/material/Stack";
 import { Tree } from "@stratakit/structures";
 
-import styles from "./Tree.default.module.css";
+import styles from "./Tree.select-many.module.css";
 
 export default () => {
-	const [expandedItems, setExpandedItems] = React.useState<
-		Record<string, boolean>
-	>({
-		item1: true,
-		item2: false,
-		item3: false,
-	});
-
-	const toggleExpanded = (itemKey: string) => {
-		setExpandedItems((prev) => ({
-			...prev,
-			[itemKey]: !prev[itemKey],
-		}));
-	};
-
-	const [selectedItems, setSelectedItems] = React.useState<
-		Record<string, boolean>
-	>({
-		item1: true,
-		"item1.1": false,
-		"item1.2": true,
-		item2: false,
-		"item2.1": false,
-		"item2.2": false,
-		item3: false,
-		"item3.1": false,
-		"item3.2": false,
-	});
-
-	const entries = Object.entries(selectedItems);
-	const selectedCount = entries.filter((item) => item[1] === true).length;
-	const allSelected = selectedCount === entries.length;
-	const noneSelected = selectedCount === 0;
-
-	const toggleSelected = (itemKey: string, itemValue?: boolean) => {
-		setSelectedItems((prev) => ({
-			...prev,
-			[itemKey]: itemValue !== undefined ? itemValue : !prev[itemKey],
-		}));
-	};
-
-	const selectAll = () => {
-		Object.keys(selectedItems).forEach((key) => {
-			toggleSelected(key, true);
-		});
-	};
-
-	const selectNone = () => {
-		Object.keys(selectedItems).forEach((key) => {
-			toggleSelected(key, false);
-		});
-	};
+	const [expandedItems, setExpandedItems] = React.useState<string[]>([
+		data[0].id,
+	]);
+	const [selectedItems, setSelectedItems] = React.useState<string[]>([
+		data[0].children[0].id,
+		data[2].id,
+	]);
 
 	return (
-		<Stack spacing={1}>
-			<ButtonGroup fullWidth aria-label="Bulk actions">
-				<Button onClick={() => selectAll()} disabled={allSelected}>
-					Select all
-				</Button>
-				<Button onClick={() => selectNone()} disabled={noneSelected}>
-					Deselect all
-				</Button>
-			</ButtonGroup>
-			<Tree.Root className={styles.tree} aria-multiselectable="true">
-				<Tree.Item
-					label="Item 1"
-					aria-level={1}
-					aria-posinset={1}
-					aria-setsize={3}
-					expanded={expandedItems.item1}
-					onExpandedChange={() => toggleExpanded("item1")}
-					selected={selectedItems.item1}
-					onSelectedChange={() => toggleSelected("item1")}
-				/>
-				{expandedItems.item1 && (
-					<>
-						<Tree.Item
-							label="Item 1.1"
-							aria-level={2}
-							aria-posinset={1}
-							aria-setsize={2}
-							selected={selectedItems["item1.1"]}
-							onSelectedChange={() => toggleSelected("item1.1")}
-						/>
-						<Tree.Item
-							label="Item 1.2"
-							aria-level={2}
-							aria-posinset={2}
-							aria-setsize={2}
-							selected={selectedItems["item1.2"]}
-							onSelectedChange={() => toggleSelected("item1.2")}
-						/>
-					</>
-				)}
-				<Tree.Item
-					label="Item 2"
-					aria-level={1}
-					aria-posinset={2}
-					aria-setsize={3}
-					expanded={expandedItems.item2}
-					onExpandedChange={() => toggleExpanded("item2")}
-					selected={selectedItems.item2}
-					onSelectedChange={() => toggleSelected("item2")}
-				/>
-				{expandedItems.item2 && (
-					<>
-						<Tree.Item
-							label="Item 2.1"
-							aria-level={2}
-							aria-posinset={1}
-							aria-setsize={2}
-							selected={selectedItems["item2.1"]}
-							onSelectedChange={() => toggleSelected("item2.1")}
-						/>
-						<Tree.Item
-							label="Item 2.2"
-							aria-level={2}
-							aria-posinset={2}
-							aria-setsize={2}
-							selected={selectedItems["item2.2"]}
-							onSelectedChange={() => toggleSelected("item2.2")}
-						/>
-					</>
-				)}
-				<Tree.Item
-					label="Item 3"
-					aria-level={1}
-					aria-posinset={3}
-					aria-setsize={3}
-					expanded={expandedItems.item3}
-					onExpandedChange={() => toggleExpanded("item3")}
-					selected={selectedItems.item3}
-					onSelectedChange={() => toggleSelected("item3")}
-				/>
-				{expandedItems.item3 && (
-					<>
-						<Tree.Item
-							label="Item 3.1"
-							aria-level={2}
-							aria-posinset={1}
-							aria-setsize={2}
-							selected={selectedItems["item3.1"]}
-							onSelectedChange={() => toggleSelected("item3.1")}
-						/>
-						<Tree.Item
-							label="Item 3.2"
-							aria-level={2}
-							aria-posinset={2}
-							aria-setsize={2}
-							selected={selectedItems["item3.2"]}
-							onSelectedChange={() => toggleSelected("item3.2")}
-						/>
-					</>
-				)}
-			</Tree.Root>
-		</Stack>
+		<Tree.Root className={styles.tree} aria-multiselectable="true">
+			{flatData.map((item) => {
+				const parentCollapsed =
+					item.parentId && !expandedItems.includes(item.parentId);
+				if (parentCollapsed) return null;
+				return (
+					<Tree.Item
+						key={item.id}
+						label={item.label}
+						aria-level={item.level}
+						aria-posinset={item.posInset}
+						aria-setsize={item.setSize}
+						expanded={
+							item.children ? expandedItems.includes(item.id) : undefined
+						}
+						onExpandedChange={(expanded) => {
+							setExpandedItems((prev) => {
+								if (expanded) {
+									return [...prev, item.id];
+								} else {
+									return prev.filter((id) => id !== item.id);
+								}
+							});
+						}}
+						selected={selectedItems.includes(item.id)}
+						onSelectedChange={(selected) => {
+							setSelectedItems((prev) => {
+								if (selected) {
+									return [...prev, item.id];
+								} else {
+									return prev.filter((id) => id !== item.id);
+								}
+							});
+						}}
+					/>
+				);
+			})}
+		</Tree.Root>
 	);
 };
+
+interface TreeItem {
+	id: string;
+	label: string;
+	expanded?: boolean;
+	children?: TreeItem[];
+}
+
+const data = [
+	{
+		id: "item1",
+		label: "Item 1",
+		children: [
+			{
+				id: "item1.1",
+				label: "Item 1.1",
+			},
+			{
+				id: "item1.2",
+				label: "Item 1.2",
+			},
+		],
+	},
+	{
+		id: "item2",
+		label: "Item 2",
+		children: [
+			{
+				id: "item2.1",
+				label: "Item 2.1",
+			},
+			{
+				id: "item2.2",
+				label: "Item 2.2",
+			},
+			{
+				id: "item2.3",
+				label: "Item 2.3",
+			},
+		],
+	},
+	{
+		id: "item3",
+		label: "Item 3",
+	},
+] as const satisfies TreeItem[];
+
+interface FlatTreeItem extends TreeItem {
+	level: number;
+	posInset: number;
+	setSize: number;
+	parentId?: string;
+}
+
+// Flattens the tree data up to two levels.
+const flatData = data.flatMap<FlatTreeItem>((item, index) => {
+	const children = "children" in item ? item.children : [];
+	const flatChildren = children.map<FlatTreeItem>((child, childIndex) => ({
+		...child,
+		level: 2,
+		posInset: childIndex + 1,
+		setSize: children.length,
+		parentId: item.id,
+	}));
+	return [
+		{
+			...item,
+			level: 1,
+			posInset: index + 1,
+			setSize: data.length,
+		},
+		...flatChildren,
+	];
+});
--- examples/structures/Tree.select-one.tsx
+++ examples/structures/Tree.select-one.tsx
@@ -5,139 +5,122 @@
 import * as React from "react";
 import { Tree } from "@stratakit/structures";
 
-import styles from "./Tree.default.module.css";
+import styles from "./Tree.select-one.module.css";
 
 export default () => {
-	const [expandedItems, setExpandedItems] = React.useState<
-		Record<string, boolean>
-	>({
-		item1: true,
-		item2: false,
-		item3: false,
-	});
-
-	const toggleExpanded = (itemKey: string) => {
-		setExpandedItems((prev) => ({
-			...prev,
-			[itemKey]: !prev[itemKey],
-		}));
-	};
-
-	const [selectedItems, setSelectedItems] = React.useState<
-		Record<string, boolean>
-	>({
-		"item1": false,
-		"item1.1": true,
-		"item1.2": false,
-		"item2": false,
-		"item2.1": false,
-		"item2.2": false,
-		"item3": false,
-		"item3.1": false,
-		"item3.2": false,
-	});
-
-	const toggleSelected = (itemKey: string) => {
-		Object.keys(selectedItems).forEach(key => {
-			setSelectedItems((prev) => ({
-				...prev,
-				[key]: key !== itemKey ? false : !prev[itemKey],
-			}));			
-		});
-	};
+	const [expandedItems, setExpandedItems] = React.useState<string[]>([
+		data[0].id,
+	]);
+	const [selectedItem, setSelectedItem] = React.useState<string | undefined>(
+		data[0].children[0].id,
+	);
 
 	return (
 		<Tree.Root className={styles.tree}>
-			<Tree.Item
-				label="Item 1"
-				aria-level={1}
-				aria-posinset={1}
-				aria-setsize={3}
-				expanded={expandedItems.item1}
-				onExpandedChange={() => toggleExpanded("item1")}
-				selected={selectedItems.item1}
-				onSelectedChange={() => toggleSelected("item1")}
-			/>
-			{expandedItems.item1 && (
-				<>
+			{flatData.map((item) => {
+				const parentCollapsed =
+					item.parentId && !expandedItems.includes(item.parentId);
+				if (parentCollapsed) return null;
+				return (
 					<Tree.Item
-						label="Item 1.1"
-						aria-level={2}
-						aria-posinset={1}
-						aria-setsize={2}
-						selected={selectedItems["item1.1"]}
-						onSelectedChange={() => toggleSelected("item1.1")}
+						key={item.id}
+						label={item.label}
+						aria-level={item.level}
+						aria-posinset={item.posInset}
+						aria-setsize={item.setSize}
+						expanded={
+							item.children ? expandedItems.includes(item.id) : undefined
+						}
+						onExpandedChange={(expanded) => {
+							setExpandedItems((prev) => {
+								if (expanded) {
+									return [...prev, item.id];
+								} else {
+									return prev.filter((id) => id !== item.id);
+								}
+							});
+						}}
+						selected={selectedItem === item.id}
+						onSelectedChange={(selected) => {
+							setSelectedItem(selected ? item.id : undefined);
+						}}
 					/>
-					<Tree.Item
-						label="Item 1.2"
-						aria-level={2}
-						aria-posinset={2}
-						aria-setsize={2}
-						selected={selectedItems["item1.2"]}
-						onSelectedChange={() => toggleSelected("item1.2")}
-					/>
-				</>
-			)}
-			<Tree.Item
-				label="Item 2"
-				aria-level={1}
-				aria-posinset={2}
-				aria-setsize={3}
-				expanded={expandedItems.item2}
-				onExpandedChange={() => toggleExpanded("item2")}
-				selected={selectedItems.item2}
-				onSelectedChange={() => toggleSelected("item2")}
-			/>
-			{expandedItems.item2 && (
-				<>
-					<Tree.Item
-						label="Item 2.1"
-						aria-level={2}
-						aria-posinset={1}
-						aria-setsize={2}
-						selected={selectedItems["item2.1"]}
-						onSelectedChange={() => toggleSelected("item2.1")}
-					/>
-					<Tree.Item
-						label="Item 2.2"
-						aria-level={2}
-						aria-posinset={2}
-						aria-setsize={2}
-						selected={selectedItems["item2.2"]}
-						onSelectedChange={() => toggleSelected("item2.2")}
-					/>
-				</>
-			)}
-			<Tree.Item
-				label="Item 3"
-				aria-level={1}
-				aria-posinset={3}
-				aria-setsize={3}
-				expanded={expandedItems.item3}
-				onExpandedChange={() => toggleExpanded("item3")}
-				selected={selectedItems.item3}
-				onSelectedChange={() => toggleSelected("item3")}
-			/>
-			{expandedItems.item3 && (
-				<>
-					<Tree.Item
-						label="Item 3.1"
-						aria-level={2}
-						aria-posinset={1}
-						aria-setsize={2}
-						selected={selectedItems["item3.1"]}
-						onSelectedChange={() => toggleSelected("item3.1")}
-					/>
-					<Tree.Item
-						label="Item 3.2"
-						aria-level={2}
-						aria-posinset={2}
-						aria-setsize={2}
-						selected={selectedItems["item3.2"]}
-						onSelectedChange={() => toggleSelected("item3.2")}
-					/>
-				</>
-			)}
+				);
+			})}
 		</Tree.Root>
 	);
 };
+
+interface TreeItem {
+	id: string;
+	label: string;
+	expanded?: boolean;
+	children?: TreeItem[];
+}
+
+const data = [
+	{
+		id: "item1",
+		label: "Item 1",
+		children: [
+			{
+				id: "item1.1",
+				label: "Item 1.1",
+			},
+			{
+				id: "item1.2",
+				label: "Item 1.2",
+			},
+		],
+	},
+	{
+		id: "item2",
+		label: "Item 2",
+		children: [
+			{
+				id: "item2.1",
+				label: "Item 2.1",
+			},
+			{
+				id: "item2.2",
+				label: "Item 2.2",
+			},
+			{
+				id: "item2.3",
+				label: "Item 2.3",
+			},
+		],
+	},
+	{
+		id: "item3",
+		label: "Item 3",
+	},
+] as const satisfies TreeItem[];
+
+interface FlatTreeItem extends TreeItem {
+	level: number;
+	posInset: number;
+	setSize: number;
+	parentId?: string;
+}
+
+// Flattens the tree data up to two levels.
+const flatData = data.flatMap<FlatTreeItem>((item, index) => {
+	const children = "children" in item ? item.children : [];
+	const flatChildren = children.map<FlatTreeItem>((child, childIndex) => ({
+		...child,
+		level: 2,
+		posInset: childIndex + 1,
+		setSize: children.length,
+		parentId: item.id,
+	}));
+	return [
+		{
+			...item,
+			level: 1,
+			posInset: index + 1,
+			setSize: data.length,
+		},
+		...flatChildren,
+	];
+});

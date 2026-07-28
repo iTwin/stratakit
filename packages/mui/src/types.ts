3671bf6335c695372bcb003a91ef6f58ba639aad
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-07-28 11:57:35 -0400

    Change SwitchOwnProps to SwitchProps

--- packages/mui/src/types.ts
+++ packages/mui/src/types.ts
@@ -956,7 +956,7 @@ declare module "@mui/material/Switch" {
 		error: false;
 	}
 
-	interface SwitchOwnProps {
+	interface SwitchProps {
 		/** @deprecated DO NOT USE. */
 		checkedIcon?: SwitchProps["checkedIcon"];
 

1d9c291c998301036079f1fb6af0283f4df10481
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-07-31 15:58:05 -0400

    disableRipple boolean instead of RadioProps["disableRipple"]

--- packages/mui/src/types.ts
+++ packages/mui/src/types.ts
@@ -489,7 +489,7 @@ declare module "@mui/material/Radio" {
 		color?: RadioProps["color"];
 
 		/** @deprecated StrataKit does not support this prop. */
-		disableRipple?: RadioProps["disableRipple"];
+		disableRipple?: boolean;
 
 		/** @deprecated StrataKit does not support this prop. */
 		icon?: RadioProps["icon"];

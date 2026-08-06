cd0ed9f1e07d5861a5cba4ac8fb4abd2644b621b
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-08-06 12:06:13 -0400

    Add disableFocusRipple and disableTouchRipple

--- packages/mui/src/types.ts
+++ packages/mui/src/types.ts
@@ -38,6 +38,7 @@ import type { SelectProps } from "@mui/material/Select";
 import type {} from "@mui/material/Slide";
 import type {} from "@mui/material/Stepper";
 import type { SvgIconProps } from "@mui/material/SvgIcon";
+import type { SwitchProps } from "@mui/material/Switch";
 import type { TabProps } from "@mui/material/Tab";
 import type { TableCellProps as MuiTableCellProps } from "@mui/material/TableCell";
 import type { TabsProps } from "@mui/material/Tabs";

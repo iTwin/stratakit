b35803e5dcfe0c09df53f892ab88b297662f9a79
Mark Dane <mark@cesium.com>
2026-08-19 14:00:31 +0000

    Fix bug with Playwright not importing .css files

--- apps/test-app/app/ScreenShotWrapper.tsx
+++ apps/test-app/app/ScreenShotWrapper.tsx
@@ -3,6 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import React from "react";
+import { SCREENSHOT_TEST_ID } from "./ScreenShotTestId.ts";
 
 import styles from "./ScreenShotWrapper.module.css";
 
@@ -28,10 +29,8 @@ export function ScreenShotWrapper({
 	}
 
 	return (
-		<div data-testid={ScreenShotWrapper.TestId} className={styles.shrinkWrap}>
+		<div data-testid={SCREENSHOT_TEST_ID} className={styles.shrinkWrap}>
 			{children}
 		</div>
 	);
 }
-
-ScreenShotWrapper.TestId = "screenshot";
--- apps/test-app/app/generateVisualTests.ts
+++ apps/test-app/app/generateVisualTests.ts
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { expect, test } from "#playwright";
-import { ScreenShotWrapper } from "./ScreenShotWrapper.tsx";
+import { SCREENSHOT_TEST_ID } from "./ScreenShotTestId.ts";
 
 /**
  * Creates tests for light, dark, and forced colors theme.  Expects the
@@ -14,18 +14,18 @@ export function generateVisualTests(path: string) {
 	test("dark", async ({ page }) => {
 		await page.emulateMedia({ colorScheme: "dark" });
 		await page.goto(path);
-		await expect(page.getByTestId(ScreenShotWrapper.TestId)).toHaveScreenshot();
+		await expect(page.getByTestId(SCREENSHOT_TEST_ID)).toHaveScreenshot();
 	});
 
 	test("light", async ({ page }) => {
 		await page.emulateMedia({ colorScheme: "light" });
 		await page.goto(path);
-		await expect(page.getByTestId(ScreenShotWrapper.TestId)).toHaveScreenshot();
+		await expect(page.getByTestId(SCREENSHOT_TEST_ID)).toHaveScreenshot();
 	});
 
 	test("forced-colors", async ({ page }) => {
 		await page.emulateMedia({ forcedColors: "active" });
 		await page.goto(path);
-		await expect(page.getByTestId(ScreenShotWrapper.TestId)).toHaveScreenshot();
+		await expect(page.getByTestId(SCREENSHOT_TEST_ID)).toHaveScreenshot();
 	});
 }

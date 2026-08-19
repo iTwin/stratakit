14a59a786b5116a2ceeda3951f1d9af890caf947
Mark Dane <mark@cesium.com>
2026-08-19 18:04:19 +0000

    Add Alert tests

---
+++ apps/test-app/app/tests/mui/Alert.spec.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { test } from "#playwright";
+import { generateVisualTests } from "~/generateVisualTests.ts";
+
+test.describe
+	.only("@visual", () => {
+		generateVisualTests("/tests/mui/Alert/Visual");
+	});

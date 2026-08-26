3ee35d89be400760b2f2a8fc44afe53830affc82
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-08-26 18:36:57 +0300

    Remove Dialog._nested example

--- apps/test-app/app/mui/Dialog.showcase.tsx
+++ apps/test-app/app/mui/Dialog.showcase.tsx
@@ -3,14 +3,8 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import DialogNested_ from "examples/mui/Dialog._nested.tsx";
 import DialogDefault from "examples/mui/Dialog.default.tsx";
 
 export default function DialogExamples() {
-	return (
-		<>
-			<DialogDefault />
-			<DialogNested_ />
-		</>
-	);
+	return <DialogDefault />;
 }

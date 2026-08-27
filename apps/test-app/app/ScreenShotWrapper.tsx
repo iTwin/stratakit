214438c0ef0459d647d3e310629ddc71410444fb
Mark Dane <mark@cesium.com>
2026-08-27 18:29:41 +0000

    Remove spacing from wrapper

--- apps/test-app/app/ScreenShotWrapper.tsx
+++ apps/test-app/app/ScreenShotWrapper.tsx
@@ -2,32 +2,17 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import React from "react";
+
 import { SCREENSHOT_TEST_ID } from "./ScreenShotTestId.ts";
 
+import type React from "react";
+
 import styles from "./ScreenShotWrapper.module.css";
-
-function SpacedComponent({ children }: React.PropsWithChildren) {
-	return <div className={styles.section}>{children}</div>;
-}
-
-interface ScreenShotWrapperProps extends React.PropsWithChildren {
-	addSpacing?: boolean;
-}
 
 /**
  * Shrink wraps children in a div with a know `data-testid`.
  */
-export function ScreenShotWrapper({
-	children,
-	addSpacing,
-}: ScreenShotWrapperProps) {
-	if (addSpacing) {
-		children = React.Children.map(children, (child) => (
-			<SpacedComponent>{child}</SpacedComponent>
-		));
-	}
-
+export function ScreenShotWrapper({ children }: React.PropsWithChildren) {
 	return (
 		<div data-testid={SCREENSHOT_TEST_ID} className={styles.shrinkWrap}>
 			{children}
--- apps/test-app/app/tests/mui/Button.spec.stories.tsx
+++ apps/test-app/app/tests/mui/Button.spec.stories.tsx
@@ -3,6 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import Stack from "@mui/material/Stack";
 import ExampleIcons from "examples/mui/Button._icons.js";
 import ExamplePermutations from "examples/mui/Button._permutations.js";
 import ExampleSizes from "examples/mui/Button.sizes.js";
@@ -10,10 +11,12 @@ import { ScreenShotWrapper } from "~/ScreenShotWrapper.tsx";
 
 export function Visual() {
 	return (
-		<ScreenShotWrapper addSpacing>
-			<ExampleIcons />
-			<ExamplePermutations />
-			<ExampleSizes />
+		<ScreenShotWrapper>
+			<Stack spacing={2}>
+				<ExampleIcons />
+				<ExamplePermutations />
+				<ExampleSizes />
+			</Stack>
 		</ScreenShotWrapper>
 	);
 }

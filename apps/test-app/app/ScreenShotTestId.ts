a42d0923ef4a6849e88db7b44d5359908dd39312
Mark Dane <mark@cesium.com>
2026-08-28 14:19:32 +0000

    grammar

--- apps/test-app/app/ScreenShotTestId.ts
+++ apps/test-app/app/ScreenShotTestId.ts
@@ -3,5 +3,5 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 // Keep this in a plain Typescript file without any .css imports so Playwright
-// (which does not have CSS imports configure) can read it.
+// (which does not have CSS imports configured) can read it.
 export const SCREENSHOT_TEST_ID = "screenshot";

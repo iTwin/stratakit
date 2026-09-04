8c223ce3463ea0344be6cd40e71a35c176f9cc71
Mark Dane <mark@cesium.com>
2026-09-04 16:42:34 +0000

    Use built in react compiler

--- internal/minimal-template/vite.config.js
+++ internal/minimal-template/vite.config.js
@@ -6,11 +6,5 @@ export default defineConfig({
 	build: {
 		assetsInlineLimit: (filePath) => !filePath.endsWith(".svg"),
 	},
-	plugins: [
-		react({
-			babel: {
-				plugins: [["babel-plugin-react-compiler"]],
-			},
-		}),
-	],
+	plugins: [react({ compiler: true })],
 });

9e98349fa5d300f7f3a9e3a367773e8db755fe61
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2026-09-18 15:38:35 -0400

    Avatar initial and icon meet contrast requirements

--- apps/test-app/app/mui/Avatar.showcase.tsx
+++ apps/test-app/app/mui/Avatar.showcase.tsx
@@ -3,18 +3,23 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import Stack from "@mui/material/Stack";
+import AvatarColors_ from "examples/mui/Avatar._colors.tsx";
 import AvatarDecorative from "examples/mui/Avatar.decorative.tsx";
 import AvatarDefault from "examples/mui/Avatar.default.tsx";
 import AvatarIcon from "examples/mui/Avatar.icon.tsx";
 import AvatarInitials from "examples/mui/Avatar.initials.tsx";
+import { isProduction } from "~/~utils.tsx";
 
 export default function AvatarExamples() {
 	return (
-		<Stack spacing={2} direction="row" sx={{ flexWrap: "wrap" }}>
-			<AvatarDefault />
-			<AvatarInitials />
-			<AvatarIcon />
-			<AvatarDecorative />
-		</Stack>
+		<>
+			<Stack spacing={2} direction="row" sx={{ flexWrap: "wrap" }}>
+				<AvatarDefault />
+				<AvatarInitials />
+				<AvatarIcon />
+				<AvatarDecorative />
+			</Stack>
+			{!isProduction && <AvatarColors_ />}
+		</>
 	);
 }

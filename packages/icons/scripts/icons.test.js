50ff27fa6dd862edbc5e2a2a9a570392cabbbd31
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-09-24 13:38:50 +0200

    Test icons-meta.json

--- packages/icons/scripts/icons.test.js
+++ packages/icons/scripts/icons.test.js
@@ -8,6 +8,8 @@ import { readdir } from "node:fs/promises";
 import test from "node:test";
 import { fileURLToPath } from "node:url";
 
+import iconsMeta from "../icons-meta.json" with { type: "json" };
+
 const ICONS_DIR = fileURLToPath(new URL("../icons", import.meta.url));
 const SRC_DIR = fileURLToPath(new URL("../src", import.meta.url));
 
@@ -22,4 +24,30 @@ test("every SVG has a matching TS module", async () => {
 		expected.toSorted(),
 		"Mismatch between `.svg` files and corresponding `.ts` modules.",
 	);
+});
+
+test("every SVG has meta data", async () => {
+	const iconMetaKeys = Object.keys(iconsMeta);
+
+	const icons = (await readdir(ICONS_DIR)).map((icon) =>
+		icon.replace(/\.svg$/, ""),
+	);
+
+	const redundantMetaIcons = iconMetaKeys.filter(
+		(metaIcon) => !icons.includes(metaIcon),
+	);
+	assert.deepEqual(redundantMetaIcons, [], "Redundant meta data found");
+
+	const iconsWithoutMeta = icons.filter((icon) => !iconsMeta[icon]);
+	assert.deepEqual(iconsWithoutMeta, [], "Icons without meta data found");
+
+	const iconsWithoutAliases = icons.filter((icon) => {
+		const iconMeta = iconsMeta[icon];
+		return (
+			!iconMeta ||
+			!Array.isArray(iconMeta.aliases) ||
+			iconMeta.aliases.length === 0
+		);
+	});
+	assert.deepEqual(iconsWithoutAliases, [], "Icons without aliases found");
 });

08fe5e875b27cbadf223852cead4e4a07260864b
Jason Wichert <82613512+jasonjwichert@users.noreply.github.com>
2026-09-24 09:07:02 -0400

    re order test app example

--- apps/test-app/app/mui/DatePicker.showcase.tsx
+++ apps/test-app/app/mui/DatePicker.showcase.tsx
@@ -14,9 +14,9 @@ export default function DatePickerExamples() {
 		<>
 			<DatePickerDefault />
 			<DatePickerSizes />
-			<DatePickerMonth />
 			<DatePickerTime />
 			<DatePickerCalendar />
+			<DatePickerMonth />
 		</>
 	);
 }

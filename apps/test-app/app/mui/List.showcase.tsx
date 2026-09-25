c669ab7a716e8fc70a099a615c34743d1f54c086
Mark Dane <mark@cesium.com>
2026-09-25 16:24:20 +0000

    Remove unneeded Box

--- apps/test-app/app/mui/List.showcase.tsx
+++ apps/test-app/app/mui/List.showcase.tsx
@@ -3,11 +3,11 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { Box, ListItemText } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 import Stack from "@mui/material/Stack";
 import { Icon } from "@stratakit/foundations";
 import ListAvatar from "examples/mui/List.avatar.tsx";
@@ -28,30 +28,28 @@ export default function ListExamples() {
 
 export function SecondaryActionPadding() {
 	return (
-		<Box sx={{ width: "240px" }}>
-			<List>
-				<ListItem
-					secondaryAction={
-						<IconButton label="Rename">
-							<Icon href={svgRename} />
-						</IconButton>
-					}
-				>
-					<ListItemText primary="Bldg 274_Architectural_0SY71309-293-31-24_RVT2022-rev2-final.rvt" />
-				</ListItem>
-				<ListItem
-					disablePadding
-					secondaryAction={
-						<IconButton label="Rename">
-							<Icon href={svgRename} />
-						</IconButton>
-					}
-				>
-					<ListItemButton>
-						<ListItemText primary="Bldg 274_Architectural_0SY71309-293-31-24_RVT2022-rev2-final.rvt.rvt" />
-					</ListItemButton>
-				</ListItem>
-			</List>
-		</Box>
+		<List sx={{ width: "240px" }}>
+			<ListItem
+				secondaryAction={
+					<IconButton label="Rename">
+						<Icon href={svgRename} />
+					</IconButton>
+				}
+			>
+				<ListItemText primary="Bldg 274_Architectural_0SY71309-293-31-24_RVT2022-rev2-final.rvt" />
+			</ListItem>
+			<ListItem
+				disablePadding
+				secondaryAction={
+					<IconButton label="Rename">
+						<Icon href={svgRename} />
+					</IconButton>
+				}
+			>
+				<ListItemButton>
+					<ListItemText primary="Bldg 274_Architectural_0SY71309-293-31-24_RVT2022-rev2-final.rvt.rvt" />
+				</ListItemButton>
+			</ListItem>
+		</List>
 	);
 }
